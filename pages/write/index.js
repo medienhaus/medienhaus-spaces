f5985324216dcd2892ca2ddec73d520861ef7f73
Marcel <marcel-klasse@users.noreply.github.com>
2022-07-14 13:29:39 +0200

    remove log

--- pages/write/index.js
+++ pages/write/index.js
@@ -174,7 +174,6 @@ export default function Write() {
         };
 
         !cancelled && serviceSpaceId && serverPads && syncServerPadsWithMatrix();
-        console.log(serverPads);
 
         return () => cancelled = true;
     }, [serviceSpaceId, serverPads]);
