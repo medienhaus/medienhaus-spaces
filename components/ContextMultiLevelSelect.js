89a65b7e114e3bfda04b65ac8ee376026151be12
Florian Weber <florian@weber.world>
2022-10-18 11:55:27 +0200

    /explore: Show the contents of a certain context (& improve the ContextMultiLevelSelect component)

--- components/ContextMultiLevelSelect.js
+++ components/ContextMultiLevelSelect.js
@@ -1,69 +1,135 @@
-import React, { useEffect, useState } from 'react';
-import getConfig from 'next/config';
+import React, { useCallback, useEffect, useState } from 'react';
+import _ from 'lodash';
 
 import { useAuth } from '../lib/Auth';
 
-const ContextBrowserLevel = ({ parentSpaceRoomId, onSelect }) => {
+const ContextMultiLevelSelectSingleLevel = ({ parentSpaceRoomId, selectedContextRoomId, onSelect, onFetchedChildren, templatePlaceholderMapping, templatePrefixFilter, sortAlphabetically, showTopics }) => {
     const auth = useAuth();
-    const [selectedContext, setSelectedContext] = useState('');
-    const [childContexts, setChildContexts] = useState({});
+    const [isLoading, setIsLoading] = useState(true);
+    const [parentSpaceMetaEvent, setParentSpaceMetaEvent] = useState();
+    const [childContexts, setChildContexts] = useState();
 
-    // Fetch all child contexts
     useEffect(() => {
-        const fetchChildContexts = async () => {
-            const roomHierarchy = await auth.getAuthenticationProvider('matrix').getMatrixClient().getRoomHierarchy(parentSpaceRoomId, undefined, 1);
-            const rooms = roomHierarchy.rooms;
-            // Remove the first entry, which is the context we retrieved the children for
-            rooms.shift();
-            // @TODO: Filter out spaces that aren't really contexts
-            setChildContexts(rooms);
+        let isSubscribed = true;
+
+        // Fetch meta event of the parent space
+        const fetchMetaEvent = async () => {
+            const metaEvent = await auth.getAuthenticationProvider('matrix').getMatrixClient().getStateEvent(parentSpaceRoomId, 'dev.medienhaus.meta').catch(() => {});
+            isSubscribed && setParentSpaceMetaEvent(metaEvent);
         };
 
-        fetchChildContexts();
-    }, [auth, parentSpaceRoomId]);
+        // Fetch all child contexts
+        const fetchChildContexts = async () => {
+            let newChildContexts = [];
+            const roomHierarchy = await auth.getAuthenticationProvider('matrix').getMatrixClient().getRoomHierarchy(parentSpaceRoomId, undefined, 1);
+            // Remove the first entry, which is the context we retrieved the children for
+            roomHierarchy.rooms.shift();
+            // Ensure we're looking at contexts, and not spaces/rooms of other types
+            for (const room of roomHierarchy.rooms) {
+                const metaEvent = await auth.getAuthenticationProvider('matrix').getMatrixClient().getStateEvent(room.room_id, 'dev.medienhaus.meta').catch(() => {});
+                // If this space/room does not have a meta event we do not care about it
+                if (!metaEvent) continue;
+                // If this is not a context, ignore this space child
+                if (metaEvent && metaEvent.type !== 'context') continue;
+                // If we only want to show specific contexts, ignore this space child if its template doesn't have the given prefix
+                if (templatePrefixFilter && metaEvent && !_.startsWith(metaEvent.template, templatePrefixFilter)) continue;
+                // ... otherwise show this space child:
+                newChildContexts.push(room);
+            }
+            if (sortAlphabetically) {
+                newChildContexts = _.sortBy(newChildContexts, 'name');
+            }
+            if (!isSubscribed) return;
+            onFetchedChildren(newChildContexts.length > 0);
+            setChildContexts(newChildContexts);
+        };
 
-    if (Object.keys(childContexts).length < 1) {
+        const fetch = async () => {
+            setIsLoading(true);
+            if (templatePlaceholderMapping) await fetchMetaEvent();
+            await fetchChildContexts();
+            setIsLoading(false);
+        };
+
+        fetch();
+
+        return () => {
+            isSubscribed = false;
+        };
+        // Do not list `onFetchedChildren` as a dependency because for some reason that will keep re-rendering this component.
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [parentSpaceRoomId, sortAlphabetically, templatePlaceholderMapping]);
+
+    if (isLoading) {
+        return <select key="loading" disabled><option>loading...</option></select>;
+    }
+
+    if (childContexts.length < 1) {
         return null;
     }
 
     return (
         <select
-            value={selectedContext}
+            value={selectedContextRoomId}
             onChange={(e) => {
-                setSelectedContext(e.target.value);
                 onSelect(parentSpaceRoomId, e.target.value);
             }}
-            style={{ width: '100%' }}
         >
-            <option />
+            {
+                (templatePlaceholderMapping && parentSpaceMetaEvent && templatePlaceholderMapping[parentSpaceMetaEvent.template]
+                // If we have a template-specific placeholder, show that...
+                    ? <option disabled value="">{ templatePlaceholderMapping[parentSpaceMetaEvent.template] }</option>
+                // ... otherwise just show an empty placeholder
+                    : <option disabled value="" />
+                )
+            }
             { Object.entries(childContexts).map(([key, room]) => (
                 <option key={key} value={room.room_id}>
                     { room.name }
+                    { showTopics && room.topic && (` (${room.topic})`) }
                 </option>
             )) }
         </select>
     );
 };
 
-const ContextMultiLevelSelect = ({ onChange }) => {
-    // An array of the currently active context breadcrumbs
-    const [activeContexts, setActiveContexts] = useState([getConfig().publicRuntimeConfig.contextRootSpaceRoomId]);
-
-    const onSelect = (parentContextRoomId, selectedChildContextRoomId) => {
-        const newActiveContexts = activeContexts.splice(0, activeContexts.findIndex((contextRoomId) => contextRoomId === parentContextRoomId) + 1);
+/**
+ * This component renders a multi-level <select> UI for an arbitrary set of contexts in the sense of Matrix spaces.
+ * `activeContexts` is the array of room IDs for the currently set context spaces.
+ *
+ * @param [Array] activeContexts
+ * @param [Func] onChange (newActiveContexts, isLeaf)
+ * @param [Bool] showTopics - If the contents of m.room.topic should be displayed in parentheses next to the name
+ * @param [Bool] sortAlphabetically - If entries should be ordered alphabetically
+ * @param [Object] templatePlaceholderMapping - Optional object containing placeholders for each <select> based on the `dev.medienhaus.meta.template` of the parent context
+ * @param [String] templatePrefixFilter - Optional prefix to filter contexts by their templates
+ */
+const ContextMultiLevelSelect = ({ activeContexts, onChange, showTopics, sortAlphabetically, templatePlaceholderMapping, templatePrefixFilter }) => {
+    const onSelect = useCallback((parentContextRoomId, selectedChildContextRoomId) => {
+        const newActiveContexts = [...activeContexts.splice(0, activeContexts.findIndex((contextRoomId) => contextRoomId === parentContextRoomId) + 1)];
         if (selectedChildContextRoomId) newActiveContexts.push(selectedChildContextRoomId);
-        setActiveContexts(newActiveContexts);
-        // Callback
-        if (onChange) onChange(newActiveContexts[newActiveContexts.length - 1]);
-    };
+        onChange(newActiveContexts, undefined);
+    }, [activeContexts, onChange]);
+
+    const onFinishedFetchingChildren = useCallback((hasChildren) => {
+        if (onChange.length > 1) {
+            onChange(activeContexts, !hasChildren);
+        }
+    }, [activeContexts, onChange]);
 
     return (
         <>
-            { activeContexts.map((contextRoomId) => (
-                <ContextBrowserLevel
+            { activeContexts && activeContexts.map((contextRoomId, i) => (
+                <ContextMultiLevelSelectSingleLevel
                     key={contextRoomId}
                     onSelect={onSelect}
+                    onFetchedChildren={onFinishedFetchingChildren}
                     parentSpaceRoomId={contextRoomId}
+                    selectedContextRoomId={activeContexts[i + 1] ?? ''}
+                    showTopics={showTopics}
+                    sortAlphabetically={sortAlphabetically}
+                    templatePlaceholderMapping={templatePlaceholderMapping}
+                    templatePrefixFilter={templatePrefixFilter}
                 />
             )) }
         </>
--- pages/explore.js
+++ pages/explore.js
@@ -1,8 +1,10 @@
-import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
+import getConfig from 'next/config';
+import { useTranslation } from 'react-i18next';
 
 import ContextMultiLevelSelect from '../components/ContextMultiLevelSelect';
+import { useAuth } from '../lib/Auth';
 
 const ExploreSection = styled.div`
   & > * + * {
@@ -15,20 +17,60 @@ const ExploreSection = styled.div`
 `;
 
 export default function Explore() {
+    const auth = useAuth();
     const { t } = useTranslation('explore');
-    const [activeContext, setActiveContext] = useState(null);
+
+    const [activeContexts, setActiveContexts] = useState([getConfig().publicRuntimeConfig.contextRootSpaceRoomId]);
+    const [contents, setContents] = useState(null);
+
+    useEffect(() => {
+        const fetchContents = async () => {
+            const contents = [];
+            const roomHierarchy = await auth.getAuthenticationProvider('matrix').getMatrixClient().getRoomHierarchy(activeContexts[activeContexts.length - 1], undefined, 1);
+            // Remove the first entry, which is the context itself
+            roomHierarchy.rooms.shift();
+            // Ignore `m.space.child` events that are empty
+            // We're only interested in the -contents- of this context, so filter out everything that's a sub-context
+            for (const room of roomHierarchy.rooms) {
+                const metaEvent = await auth.getAuthenticationProvider('matrix').getMatrixClient().getStateEvent(room.room_id, 'dev.medienhaus.meta').catch(() => {});
+                if (!metaEvent || (metaEvent && metaEvent.type !== 'context')) {
+                    // This is a valid content item we want to show
+                    contents.push({
+                        ...room,
+                        type: metaEvent?.type ?? 'chat', // If there's no meta.type we assume it's a chat room
+                        template: metaEvent?.template,
+                    });
+                }
+            }
+            setContents(contents);
+        };
+
+        if (activeContexts) fetchContents();
+    }, [activeContexts, auth]);
 
     return (
         <>
             <h1>/explore</h1>
             <ExploreSection>
-                <ContextMultiLevelSelect onChange={setActiveContext} />
-                <p>
-                    <span>{ t('Selected context') }: </span>
-                    <pre>{ activeContext }</pre>
-                </p>
+                <ContextMultiLevelSelect onChange={setActiveContexts} activeContexts={activeContexts} />
+                { (contents && contents.length > 0) ? (
+                    <div>
+                        <ol>
+                            { contents.map((content) => (
+                                <li>
+                                    { content.name }
+                                    <small>
+                                        { content.type && (<> (type: <code>{ content.type }</code>)</>) }
+                                        { content.template && (<> (template: <code>{ content.template }</code>)</>) }
+                                    </small>
+                                </li>
+                            )) }
+                        </ol>
+                    </div>
+                ) : (
+                    <p>- { t('There are no contents for this context') } -</p>
+                ) }
             </ExploreSection>
         </>
     );
 }
-
