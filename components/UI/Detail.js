985c7a1cbb7d4d43f23c2b7b362b9991efe65f79
Karen <karen.czock@bsbkoeln.de>
2022-11-01 18:06:08 +0100

    created: Detail.js

---
+++ components/UI/Detail.js
@@ -0,0 +1,10 @@
+import styled from 'styled-components';
+
+const Button = styled.detail`
+  display: block;
+  height: 3rem;
+  background: white;
+  border: 2px solid black;
+`;
+
+export default Button;
