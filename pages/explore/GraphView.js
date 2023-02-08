6b2393eb09813b44f25991c463371d2bf75a2547
Marcel <marcel-klasse@users.noreply.github.com>
2023-02-08 14:22:32 +0100

    add api to explore and improve d3js functions

--- pages/explore/GraphView.js
+++ pages/explore/GraphView.js
@@ -1,143 +1,202 @@
 
-import React from 'react';
+import React, { useEffect, useRef, useState } from 'react';
 import * as d3 from 'd3';
-import { interpolateRainbow, partition, quantize, scaleOrdinal, select } from 'd3';
+import { partition } from 'd3';
+
+import LoadingSpinner from '../../components/UI/LoadingSpinner';
+
 // import { partition } from 'd3';
 // import { format } from 'd3';
 
-function GraphView({ data, callback, parsedWidth }) {
-    console.log(parsedWidth);
-    const svgRef = React.useRef(null);
-    const format = d3.format(',d');
+function GraphView({ parsedData, callback, parsedWidth, parsedHeight, selectedNode }) {
+    const [data, setData] = useState(parsedData);
+    const [height, setHeight] = useState();
+    const svgRef = useRef(null);
 
-    function rectHeight(d) {
-        return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
-    }
-
-    function labelVisible(d) {
-        return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
-    }
-
-    const width = parsedWidth || window.innerWidth;
-    const height = window.innerHeight;
-    console.log(window);
-
-    function iciclePartition(data) {
-        const root = d3
-            .hierarchy(data)
-            .sum((d) => d.value)
-            .sort(
-                (a, b) => b.height - a.height || b.value - a.value,
-            );
-        return partition().size([
-            height,
-            ((root.height + 1) * width) / 3,
-        ])(root);
-    }
-
-    const color = scaleOrdinal(
-        quantize(interpolateRainbow, data.children.length + 1),
-    );
-    const root = iciclePartition(data);
-    let focus = root;
-
-    const svg = select('body')
-        .append('svg')
-        // .attr('viewBox', [root.x1, 0, width, height])
-        .style('font', '10px sans-serif');
-
-    const cell = svg
-        .selectAll('g')
-        .data(root.descendants())
-        .join('g')
-        .attr('transform', (d) => `translate(${d.y0},${d.x0})`);
-
-    const rect = cell
-        .append('rect')
-        .attr('width', (d) => d.y1 - d.y0 - 1)
-        .attr('height', (d) => rectHeight(d))
-        // .attr('fill-opacity', 1)
-        .attr('fill', 'var(--color-background-sidebar)')
-        .attr('stroke', 'var(--color-foreground)')
-        .attr('stroke-width', '10')
-        .style('cursor', 'pointer')
-        .on('click', clicked);
-
-    const text = cell
-        .append('text')
-        .style('user-select', 'none')
-        .attr('pointer-events', 'none')
-        .attr('x', 20)
-        .attr('y', 50)
-        .attr('font-size', '36px')
-        .attr('fill', 'var(--color-foreground)')
-        .attr('fill-opacity', (d) => +labelVisible(d));
-
-    text.append('tspan').text((d) => d.data.name);
-
-    const tspan = text
-        .append('tspan')
-        .attr('fill-opacity', (d) => labelVisible(d) * 0.7)
-        .text((d) => ` ${format(d.value)}`);
-
-    cell.append('title').text(
-        (d) =>
-            `${d
-                .ancestors()
-                .map((d) => d.data.name)
-                .reverse()
-                .join('/')}\n${format(d.value)}`,
-    );
-    console.log(root);
-    async function clicked(event, p) {
-        focus = focus === p ? (p = p.parent) : p;
-        if (!p) return;
-
-        root.each(
-            (d) =>
-                (d.target = {
-                    x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
-                    x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
-                    y0: d.y0 - p.y0,
-                    y1: d.y1 - p.y0,
-                }),
-        );
-
-        const t = cell
-            .transition()
-            .duration(750)
-            .attr(
-                'transform',
-                (d) => `translate(${d.target.y0},${d.target.x0})`,
-            );
-
-        rect
-            .transition(t)
-            .attr('height', (d) => rectHeight(d.target));
-        text
-            .transition(t)
-            .attr('fill-opacity', (d) => +labelVisible(d.target));
-        tspan
-            .transition(t)
-            .attr(
-                'fill-opacity',
-                (d) => labelVisible(d.target) * 0.7,
-            );
-
-        callback(p);
-    }
-
-    React.useEffect(() => {
-        if (svgRef.current) {
-            svgRef.current.appendChild(svg.node());
+    useEffect(() => {
+        let cancelled = false;
+        if (!cancelled) {
+            setData(parsedData);
         }
 
         return () => {
-            svgRef.current = null;
+            cancelled = true;
         };
-    }, [svg]);
+    }, [parsedData]);
 
-    return <svg ref={svgRef} width="100%" height="100%" />;
+    useEffect(() => {
+        let cancelled = false;
+
+        if (!cancelled) {
+            console.log(parsedHeight);
+            parsedHeight && setHeight(parsedHeight);
+        }
+        return () => {
+            cancelled = true;
+        };
+    }, [parsedHeight]);
+
+    const drawSvg = (data, height) => {
+        const format = d3.format(',d');
+
+        function rectHeight(d) {
+            return d.x1 - d.x0 - Math.min(1, (d.x1 - d.x0) / 2);
+        }
+
+        function labelVisible(d) {
+            return d.y1 <= width && d.y0 >= 0 && d.x1 - d.x0 > 16;
+        }
+
+        const width = parsedWidth || window.innerWidth;
+        // const height = parsedHeight;
+
+        function iciclePartition(data) {
+            const root = d3
+                .hierarchy(data)
+                .sum((d) => {
+                    return d.value || 1;
+                })
+                .sort(
+                    (a, b) => b.height - a.height || b.value - a.value,
+                );
+            return partition().size([
+                height,
+                ((root.height + 1) * width / 3),
+            ])(root);
+        }
+
+        // const color = scaleOrdinal(
+        //     quantize(interpolateRainbow, data.children.length + 1),
+        // );
+
+        const root = iciclePartition(data);
+        console.log(root);
+        let focus = root;
+
+        // const svg = select('body')
+        //     .append('svg')
+        //     // .attr('viewBox', [root.x1, 0, width, height])
+        //     .style('font', '10px sans-serif');
+
+        const svg = d3.select(svgRef.current)
+        // .attr('height', height)
+        // .attr('viewBox', [-width / 2, -height / 2, width, height])
+            .attr('style', 'max-width: 100%; height: auto; height: intrinsic;')
+            .style('font', '10px sans-serif');
+
+        const cell = svg
+            .selectAll('g')
+            .data(root.descendants())
+            .join(
+                enter => enter.append('g'),
+                update => update.
+                    style('display', (d) => console.log(d)),
+                exit => exit.remove(),
+            )
+            .attr('transform', (d) => `translate(${d.y0},${d.x0})`)
+            .attr('width', (d) => d.y1 - d.y0 - 1);
+
+        const rect = cell
+            .append('rect')
+            .attr('width', (d) => d.y1 - d.y0 - 1)
+            .attr('height', (d) => rectHeight(d))
+            .attr('fill', (d) => {
+                if (d.data.template === 'write') return 'red';
+                if (d.data.template === 'sketch') return 'blue';
+                if (d.data.type === 'item') return 'green';
+
+                return 'var(--color-background-sidebar)';
+            })
+            .attr('stroke', 'var(--color-foreground)')
+            .attr('stroke-width', '8')
+            .style('cursor', 'pointer')
+            .on('click', clicked);
+
+        const text = cell
+            .append('text')
+            .style('user-select', 'none')
+            .attr('pointer-events', 'none')
+            .attr('x', 15)
+            .attr('y', 30)
+            .attr('font-size', '1.2rem')
+            .attr('fill', 'var(--color-foreground)')
+            .attr('fill-opacity', (d) => +labelVisible(d));
+
+        text.append('tspan').text((d) => d.data.name);
+
+        // const tspan = text
+        //     .append('tspan')
+        //     .attr('fill-opacity', (d) => labelVisible(d) * 0.7)
+        //     .text((d) => ` ${format(d.value)}`);
+
+        cell.append('title').text(
+            (d) =>
+                `${d
+                    .ancestors()
+                    .map((d) => d.data.name)
+                    .reverse()
+                    .join('/')}\n${format(d.value)}`,
+        );
+
+        async function clicked(event, p) {
+            focus = focus === p ? (p = p.parent) : p;
+            if (!p) return;
+            await callback(p);
+
+            root.each(
+                (d) =>
+                    (d.target = {
+                        x0: ((d.x0 - p.x0) / (p.x1 - p.x0)) * height,
+                        x1: ((d.x1 - p.x0) / (p.x1 - p.x0)) * height,
+                        y0: d.y0 - p.y0,
+                        y1: d.y1 - p.y0,
+                    }),
+            );
+
+            const t = cell
+                .transition()
+                .duration(750)
+                .attr(
+                    'transform',
+                    (d) => `translate(${d.target.y0},${d.target.x0})`,
+                );
+
+            rect
+                .transition(t)
+                .attr('height', (d) => rectHeight(d.target));
+            text
+                .transition(t)
+                .attr('fill-opacity', (d) => +labelVisible(d.target));
+
+            // tspan
+            //     .transition(t)
+            //     .attr(
+            //         'fill-opacity',
+            //         (d) => labelVisible(d.target) * 0.7,
+            //     );
+        }
+    };
+    useEffect(() => {
+        drawSvg(data, height);
+        return () => {
+            console.log('unmount svg');
+            // svgRef.current = null;
+        };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [data, height]);
+
+    // useEffect(() => {
+    //     // if (svgRef.current) {
+    //     //     console.log(svg);
+    //     //     svgRef.current.appendChild(svg.node());
+    //     // }
+
+    //     return () => {
+    //         svgRef.current = null;
+    //     };
+    // }, [svg, height]);
+    if (!height) return <LoadingSpinner />;
+    return <svg ref={svgRef} width="100%" height={height} />;
 }
 
 export default GraphView;
--- pages/explore/index.js
+++ pages/explore/index.js
@@ -1,11 +1,13 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useRef, useState } from 'react';
 // import styled from 'styled-components';
 import getConfig from 'next/config';
+// import { useTranslation } from 'react-i18next';
 
 // import ContextMultiLevelSelect from '../../components/ContextMultiLevelSelect';
 // import { useAuth } from '../../lib/Auth';
+
 import GraphView from './GraphView';
-// import jsonData from '../../assets/hierarchy.json';
+import jsonData from '../../assets/hierarchy.json';
 import IframeLayout from '../../components/layouts/iframe';
 import LoadingSpinner from '../../components/UI/LoadingSpinner';
 
@@ -22,9 +24,43 @@ import LoadingSpinner from '../../components/UI/LoadingSpinner';
 export default function Explore() {
     const [activeContexts] = useState([getConfig().publicRuntimeConfig.contextRootSpaceRoomId]);
     const [graphObject, setGraphObject] = useState(null);
-    const [selectedNode, setSelectedNode] = useState('');
+    const [selectedNode, setSelectedNode] = useState(null);
+    const [d3Height, setD3Height] = useState();
+    const dimensionsRef = useRef();
 
-    const fetchContents = useCallback(async () => {
+    async function callApiAndAddToObject(roomId) {
+        console.log(roomId);
+        function findObject(structure, id) {
+            let ret;
+            // base case
+            if (structure.id === id) {
+                return structure;
+            } else {
+                // recursion
+                structure.children?.forEach(child => {
+                    if (!ret) {
+                        const c = findObject(child, id);
+                        if (c) ret = c;
+                    }
+                });
+            }
+            return ret;
+        }
+
+        const response = await fetch(`${getConfig().publicRuntimeConfig.authProviders.matrix.api}/api/v2/${roomId}`).catch(error => console.log(error));
+        if (!response?.ok) return;
+        const data = await response.json();
+        data.children = data.item;
+        data.children.push(...data.context);
+        setGraphObject(prevTree => {
+            const newTree = { ...prevTree };
+            const foundObject = findObject(newTree, roomId);
+            foundObject.children = data?.children;
+            return newTree;
+        });
+    }
+
+    const fetchContents = useCallback(async (roomId) => {
         // const contents = [];
         // const roomHierarchy = await auth.getAuthenticationProvider('matrix').getMatrixClient().getRoomHierarchy(activeContexts[activeContexts.length - 1], undefined, 1);
         // // Remove the first entry, which is the context itself
@@ -51,30 +87,59 @@ export default function Explore() {
         // });
 
         // initial fetch of object
-        const object = await fetch('http://192.168.0.50:3009/api/v2/!gBzMkmAvxvlPEwlvdq:moci.space/render/d3/fulltree').catch((err) => console.error(err));
-        const json = await object.json();
-        console.log(json);
-        setGraphObject(json);
+        console.log('fetching object');
+        console.log(getConfig().publicRuntimeConfig);
+        const object = await fetch(getConfig().publicRuntimeConfig.authProviders.matrix.api + '/api/v2/' + roomId).catch((err) => console.error(err));
+        // const object = await fetch('http://192.168.0.50:3009/api/v2/!gBzMkmAvxvlPEwlvdq:moci.space/render/d3/fullTree').catch((err) => console.error(err));
+        console.log('object: ');
+        console.log(object);
+
+        if (object?.ok) {
+            const json = await object.json();
+            json.children = json.item;
+            json.children.push(...json.context);
+            setGraphObject(json);
+        } else {
+            console.log('else:');
+            setGraphObject(jsonData);
+        }
     }, []);
 
     useEffect(() => {
-        if (activeContexts) fetchContents();
+        console.log(activeContexts);
+        if (activeContexts) fetchContents(activeContexts[0]);
     }, [activeContexts, fetchContents]);
 
-    const handleClicked = (element) => {
+    useEffect(() => {
+        // 8 for border
+        dimensionsRef.current && setD3Height(document.querySelector('main').offsetHeight - (8 + dimensionsRef?.current?.offsetHeight + (dimensionsRef?.current?.offsetTop * 2)+ parseInt(window.getComputedStyle(dimensionsRef.current).marginBottom)));
+        dimensionsRef.current && console.log(window.getComputedStyle(dimensionsRef.current));
+    }, [graphObject]);
+
+    const handleClicked = async (element) => {
         // element is the last node clicked on by the user
         if (!element) return;
-        console.log(element);
-        setSelectedNode(element.data.url); // if selected node is not undefined iframe loads the url(type string) from selectedNode
+        if (element.children) return;
+        await callApiAndAddToObject(element.data.id);
+        if (element.data.url) {
+            console.log('inside if');
+            await new Promise(res => setTimeout(res, 650)); //transition time in d3js minus 100ms
+            setSelectedNode(element.data.url); // if selected node is not undefined iframe loads the url(type string) from selectedNode
+            return;
+        }
+        setSelectedNode(null);
     };
 
-    if (!graphObject) return <LoadingSpinner />;
+    if (!graphObject || typeof window === 'undefined') return <LoadingSpinner />;
 
     return (
         <>
-            <IframeLayout.Sidebar width="75rem">
-                <h2>/explore</h2>
-                <GraphView data={graphObject} callback={handleClicked} />
+            <IframeLayout.Sidebar width={selectedNode ? '50%' : '100%'}>
+                <h2 ref={dimensionsRef}>/explore</h2>
+                <GraphView parsedData={graphObject}
+                    callback={handleClicked}
+                    parsedHeight={d3Height}
+                    selectedNode={selectedNode} />
             </IframeLayout.Sidebar>
             { selectedNode &&
 
