38bcf861a6486df95027634f15bb2a23e10530e5
Marcel <marcel-klasse@users.noreply.github.com>
2023-05-05 14:43:22 +0200

    adaot wording and add title as optional parameter

--- components/UI/CopyToClipboard.js
+++ components/UI/CopyToClipboard.js
@@ -4,20 +4,20 @@ import { useTranslation } from 'react-i18next';
 import Clipboard from '../../assets/icons/clipboard.svg';
 import TextButton from './TextButton';
 
-const CopyToClipboard = ({ content }) => {
-    const [contentCopied, setContentCopied] = useState(false);
+const CopyToClipboard = ({ content, title }) => {
+    const [isContentCopied, setIsContentCopied] = useState(false);
     const { t } = useTranslation();
 
     const copyToClipboard = async () => {
         navigator.clipboard.writeText(content);
-        setContentCopied(true);
+        setIsContentCopied(true);
         await new Promise(r => setTimeout(r, 2000));
-        setContentCopied(false);
+        setIsContentCopied(false);
     };
 
     return (
-        <TextButton title={t('Copy link to clipboard')} onClick={copyToClipboard}>
-            { contentCopied ?
+        <TextButton title={ title || t('Copy link to clipboard')} onClick={copyToClipboard}>
+            { isContentCopied ?
                 '✓':
                 <Clipboard fill="var(--color-fg)" /> }
         </TextButton>
