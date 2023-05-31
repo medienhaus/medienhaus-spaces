c1c473835cee27ba38799a242ba9a097b9122c5e
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-05-31 11:31:55 +0200

    add public/manifest.webmanifest && public/assets/img/favicon-512.png

--- pages/_app.js
+++ pages/_app.js
@@ -33,6 +33,7 @@ export default function App({ Component, pageProps }) {
                 <meta name="viewport" content="width=device-width, initial-scale=1.0" />
                 <meta name="apple-mobile-web-app-capable" content="yes" />
                 <meta name="mobile-web-app-capable" content="yes" />
+                <link rel="manifest" href="./manifest.webmanifest" />
                 <link rel="apple-touch-icon" href="./assets/icons/apple-touch-icon.png" />
                 <link rel="icon" type="image/x-icon" href="./favicon.ico" sizes="16x16 32x32" />
                 <link rel="icon" type="image/svg+xml" href="./favicon.svg" sizes="any" />
