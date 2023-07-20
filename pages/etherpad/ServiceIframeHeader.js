3588b7b8195ca91cd5383403047cd3412bb4544a
Marcel <marcel-klasse@users.noreply.github.com>
2023-07-20 15:10:41 +0200

    add manage context action icon

--- pages/etherpad/ServiceIframeHeader.js
+++ pages/etherpad/ServiceIframeHeader.js
@@ -4,8 +4,9 @@ import IframeLayout from '../../components/layouts/iframe';
 import BinIcon from '../../assets/icons/bin.svg';
 import CopyToClipboard from '../../components/UI/CopyToClipboard';
 import LoadingSpinnerInline from '../../components/UI/LoadingSpinnerInline';
+import ListSettingsIcon from '../../assets/icons/list-settings.svg';
 
-const ServiceIframeHeader = ({ isDeletingPad, deletePad, title, mypadsPadObject, content }) => {
+const ServiceIframeHeader = ({ isDeletingPad, deletePad, title, mypadsPadObject, content, hasManageContextActionRights, setManageContextActionToggle }) => {
     const { t } = useTranslation('write');
 
     return (
@@ -16,6 +17,13 @@ const ServiceIframeHeader = ({ isDeletingPad, deletePad, title, mypadsPadObject,
                 <button title={t(mypadsPadObject ? 'Delete pad' : 'Remove pad from my library')} onClick={deletePad}>
                     { isDeletingPad ? <LoadingSpinnerInline /> : <BinIcon fill="var(--color-foreground)" /> }
                 </button>
+                { hasManageContextActionRights && <button>
+                    <ListSettingsIcon
+                        title={t('Manage the selected context')}
+                        fill="var(--color-foreground)"
+                        onClick={() => setManageContextActionToggle(prevState => !prevState)}
+                    />
+                </button> }
             </IframeLayout.IframeHeaderButtonWrapper>
         </IframeLayout.IframeHeader>
     );
