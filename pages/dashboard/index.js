837e78c8f7392de5fe56a79a9dc6e832422f6a7f
Marcel <marcel-klasse@users.noreply.github.com>
2023-08-16 15:43:44 +0200

    refactor with logic from `ApplicationsSection`

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -1,3 +1,7 @@
+import React, { useEffect, useMemo, useRef, useState } from 'react';
+import getConfig from 'next/config';
+import _ from 'lodash';
+
 import { useAuth } from '../../lib/Auth';
 import { useMatrix } from '../../lib/Matrix';
 import ApplicationSection from './ApplicationSection';
@@ -5,13 +9,80 @@ import ApplicationSection from './ApplicationSection';
 export default function Dashboard() {
     const auth = useAuth();
     const matrix = useMatrix(auth.getAuthenticationProvider('matrix'));
+    const invites = useMemo(() => [...matrix.invites.values()], [matrix.invites]);
+
+    const matrixClient = auth.getAuthenticationProvider('matrix').getMatrixClient();
+    const [applicationInvites, setApplicationInvites] = useState({});
+    const serviceSpaces = matrix.serviceSpaces;
+
+    const authProviders = getConfig().publicRuntimeConfig.authProviders; // get the authProvider object from the config
+    const providersWithTemplates = useMemo(() => {
+        return _.pickBy(authProviders, provider => _.has(provider, 'templates')); // only return authProviders which have a templates key
+    }, [authProviders]);
+
+    useEffect(() => {
+        if (_.isEmpty(serviceSpaces) || !matrix.initialSyncDone) return;
+
+        const getMetaEventAndSort = async () => {
+            const allInvitations = { ...applicationInvites };
+
+            for (const space of invites) {
+                const meta = await matrixClient.http.authedRequest('GET', `/rooms/${space.roomId}/state/dev.medienhaus.meta`, {}, undefined, {});
+                if (!meta) continue;
+
+                Object.keys(serviceSpaces).forEach(applicationName => {
+                    const applicationTemplates = providersWithTemplates[applicationName]?.templates;
+                    if (!applicationTemplates) return;
+
+                    if (applicationTemplates.includes(meta.template)) {
+                        if (!allInvitations[applicationName]) {
+                            allInvitations[applicationName] = [];
+                        }
+
+                        if (!allInvitations[applicationName].some(invitation => invitation.roomId === space.roomId)) {
+                            allInvitations[applicationName].push(space);
+                        }
+                    }
+                });
+            }
+
+            setApplicationInvites(allInvitations);
+        };
+
+        getMetaEventAndSort();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [matrix.initialSyncDone, matrixClient.http, serviceSpaces, invites, providersWithTemplates]);
+
+    // functions which interact with matrix server
+    const rejectMatrixInvite = async (e, roomId) => {
+        e.preventDefault();
+        await matrixClient.leave(roomId);
+        setApplicationInvites(prevState => _.remove(prevState, (c) => {return c.roomId === roomId; }));
+    };
+
+    const acceptMatrixInvite = async (e, roomId) => {
+        e.preventDefault();
+        await matrixClient.joinRoom(roomId);
+        _.remove(applicationInvites, (c) => {return c.roomId === roomId; });
+    };
 
     return (
         <>
             <h2>/dashboard</h2>
             {
-                _.map(matrix.serviceSpaces, (id, name) => {
-                    return <ApplicationSection applicationId={id} name={name} />;
+                _.map(serviceSpaces, (id, name) => {
+                    if (!applicationInvites[name]) return null; // if there is no key fot the space within the invitation object. We
+                    console.log(name);
+
+                    return <ApplicationSection
+                        key={id}
+                        applicationId={id}
+                        name={name}
+                        invitations={applicationInvites[name]}
+                        acceptMatrixInvite={acceptMatrixInvite}
+                        rejectMatrixInvite={rejectMatrixInvite}
+
+                    />;
                 })
             }
         </>
