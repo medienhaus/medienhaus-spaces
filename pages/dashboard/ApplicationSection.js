356a0f86f6549ef980cc074d91e9bd30d748aebc
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-08-23 15:43:44 +0200

    add table head for invititations on /dashboard

--- pages/dashboard/ApplicationSection.js
+++ pages/dashboard/ApplicationSection.js
@@ -4,6 +4,8 @@ import getConfig from 'next/config';
 
 // import { useAuth } from '../../lib/Auth';
 // import { useMatrix } from '../../lib/Matrix';
+import { useTranslation } from 'react-i18next';
+
 import DisplayInvitations from './DisplayInvitations';
 // import DisplayLatestLinks from './DisplayLatestLinks';
 import { ServiceTable } from '../../components/UI/ServiceTable';
@@ -34,6 +36,8 @@ const ApplicationSection = ({ service, id, invitations, acceptMatrixInvite, decl
     const name = getConfig().publicRuntimeConfig.authProviders[service].path || service;
     const serviceTemplates = getConfig().publicRuntimeConfig.authProviders[service].templates;
 
+    const { t } = useTranslation('dashboard');
+
     // const applicationChildren = applicationSpace?.children.map((childId) => {
     //     return matrix.spaces.get(childId) || matrix.rooms.get(childId);
     // });
@@ -47,17 +51,35 @@ const ApplicationSection = ({ service, id, invitations, acceptMatrixInvite, decl
             </ApplicationSegment> } */ }
             <ApplicationSegment>
                 <ServiceTable>
-                    { _.map([...invitations.values()], (invite) => {
-                        if (!serviceTemplates.includes(invite.meta.template)) return null; // only display invitations from the current service
+                    <ServiceTable.Head>
+                        <ServiceTable.Row>
+                            <ServiceTable.Header align="left">
+                                <span>{ t('Invitations') }</span>
+                            </ServiceTable.Header>
+                            <ServiceTable.Header align="left">
+                                <span>{ t('From') }</span>
+                            </ServiceTable.Header>
+                            <ServiceTable.Header align="center">
+                                <span>{ t('Accept') }</span>
+                            </ServiceTable.Header>
+                            <ServiceTable.Header align="center">
+                                <span>{ t('Decline') }</span>
+                            </ServiceTable.Header>
+                        </ServiceTable.Row>
+                    </ServiceTable.Head>
+                    <ServiceTable.Body>
+                        { _.map([...invitations.values()], (invite) => {
+                            if (!serviceTemplates.includes(invite.meta.template)) return null; // only display invitations from the current service
 
-                        return <DisplayInvitations
-                            key={invite.roomId}
-                            service={service}
-                            name={name}
-                            invite={invite}
-                            declineMatrixInvite={declineMatrixInvite}
-                            acceptMatrixInvite={acceptMatrixInvite} />;
-                    }) }
+                            return <DisplayInvitations
+                                key={invite.roomId}
+                                service={service}
+                                name={name}
+                                invite={invite}
+                                declineMatrixInvite={declineMatrixInvite}
+                                acceptMatrixInvite={acceptMatrixInvite} />;
+                        }) }
+                    </ServiceTable.Body>
                 </ServiceTable>
             </ApplicationSegment>
 
