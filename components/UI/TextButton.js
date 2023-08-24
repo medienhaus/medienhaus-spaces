282ffcfa51524b86d3659d8580d6852391095d6d
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-08-24 16:42:24 +0200

    unset TextButton width and center horizontally in parent

--- components/UI/TextButton.js
+++ components/UI/TextButton.js
@@ -3,8 +3,10 @@ import styled from 'styled-components';
 const TextButton = styled.button`
   display: grid;
   place-content: center;
+  width: unset;
   height: unset;
   padding: 0;
+  margin: 0 auto;
   font: inherit;
   color: var(--color-foreground);
   cursor: pointer;
