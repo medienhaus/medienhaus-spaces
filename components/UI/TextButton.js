adbfa0dc015954a84d5ea3a1c366fda30c7af808
Marcel <marcel-klasse@users.noreply.github.com>
2023-08-29 15:15:36 +0200

    fix styling

--- components/UI/TextButton.js
+++ components/UI/TextButton.js
@@ -1,12 +1,15 @@
 import styled from 'styled-components';
 
 const TextButton = styled.button`
+  display: grid;
+  place-content: center;
+  width: unset;
+  height: unset;
   padding: 0;
+  margin: 0 auto;
   font: inherit;
   cursor: pointer;
   background: none;
-  border: none;
-  outline: inherit;
 
   &:disabled {
     color: var(--color-disabled);
