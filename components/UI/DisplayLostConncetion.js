e803ed6049ce412455aee1b86912043d31ccccc9
Marcel <marcel-klasse@users.noreply.github.com>
2023-08-29 13:48:56 +0200

    Display user feedback as soon as the connection to the matrix server is lost

---
+++ components/UI/DisplayLostConncetion.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import styled from 'styled-components';
+import { useTranslation } from 'react-i18next';
+
+import { useAuth } from '../lib/Auth';
+import { useMatrix } from '../lib/Matrix';
+import LoadingSpinnerInline from './UI/LoadingSpinnerInline';
+
+const NoConnectionView = styled.div`
+  position: absolute;
+  right: var(--margin);
+  bottom: var(--margin);
+  display: grid;
+  grid-auto-flow: column;
+  grid-gap: calc(var(--margin)/2);
+  place-items: center;
+  padding: calc(var(--margin)/2) var(--margin);
+  color: white;
+  text-transform: uppercase;
+  background-color: #ff2f4f;
+  border-radius: 4px;
+
+`;
+const DisplayLostConnection = (params) => {
+    const auth = useAuth();
+    const matrix = useMatrix(auth.getAuthenticationProvider('matrix'));
+    const { t } = useTranslation();
+
+    if (matrix.isConnectedToServer) return null;
+
+    return (
+        <NoConnectionView>
+            <small>
+                { t('Waiting for server') }
+            </small>
+            <LoadingSpinnerInline inverted />
+        </NoConnectionView>
+    );
+};
+
+export default DisplayLostConnection;
