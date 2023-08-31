c19adf8a2991bdbf558850f6663ce3a8a2582acf
Marcel <marcel-klasse@users.noreply.github.com>
2023-08-31 12:48:57 +0200

    refactor functions and add error handleing

--- pages/dashboard/DisplayInvitations.js
+++ pages/dashboard/DisplayInvitations.js
@@ -14,45 +14,49 @@ import LoadingSpinnerInline from '../../components/UI/LoadingSpinnerInline';
  *
  * @param {Object} invite — object of the room the user was invited to
  * @param {String} service — name of the service (i.e. the object key names of the 'authProvider' object in the config)
- * @param {String} name — name of the Application (i.e. the 'path' variable in the config)
+ * @param {String} path — name of the Application (i.e. the 'path' variable in the config)
  *
  * @callback acceptMatrixInvite - callback function to accept invitations to a matrix room or space
- * @param e - event triggered by the button
  * @param roomId - matrix roomId
  * @param service - name of the service (parsed to the function)
  *
  * @callback declineMatrixInvite - callback function to decline invitations to a matrix room or space
- * @param e - event triggered by the button
  * @param roomId - matrix roomId
  *
  * @returns {React.ReactComponent}
 */
 
-const DisplayInvitations = ({ invite, name, service, acceptMatrixInvite, declineMatrixInvite }) => {
+const DisplayInvitations = ({ invite, path, service, acceptMatrixInvite, declineMatrixInvite }) => {
     const { t } = useTranslation('dashboard');
     const [isAcceptingInvite, setIsAcceptingInvite] = useState(false);
-    const [isDecliningInvite, setisDecliningInvite] = useState(false);
+    const [isDecliningInvite, setIsDecliningInvite] = useState(false);
     const router = useRouter();
 
     const handleDecline = async (e, roomId) => {
-        setisDecliningInvite(true);
-        await declineMatrixInvite(e, roomId);
-        setisDecliningInvite(false);
+        e.preventDefault();
+        setIsDecliningInvite(true);
+        await declineMatrixInvite(roomId);
+        setIsDecliningInvite(false);
     };
 
     const handleAccept = async (e, roomId) => {
+        e.preventDefault();
         setIsAcceptingInvite(true);
-        await acceptMatrixInvite(e, roomId, service);
+        const forwardingUrl = await acceptMatrixInvite(roomId, service);
         setIsAcceptingInvite(false);
-        if (confirm('You’ve successfully accepted the invitation!\n\nWould you like to be redirected to the newly accepted {{name}} item?', { name: name })) {
-            router.push(`${name}/${roomId}`);
+        if (forwardingUrl) {
+            if (confirm('You’ve successfully accepted the invitation!\n\nWould you like to be redirected to the newly accepted {{name}} item?', { name: path })) {
+                router.push(forwardingUrl);
+            }
+        } else {
+            alert(t('Something went wrong! Please try again.'));
         }
     };
 
     return (
         <ServiceTable.Row>
             <ServiceTable.Cell>
-                { name }
+                { path }
             </ServiceTable.Cell>
             <ServiceTable.Cell>
                 { invite.name }
@@ -61,13 +65,13 @@ const DisplayInvitations = ({ invite, name, service, acceptMatrixInvite, decline
                 { invite.inviter?.displayName }
             </ServiceTable.Cell>
             <>
-                <ServiceTable.Cell title={t('accept invitation')} onClick={(e) => { handleAccept(e, invite.roomId); }}>
-                    <TextButton disabled={isDecliningInvite || isAcceptingInvite}>
+                <ServiceTable.Cell title={t('accept invitation')}>
+                    <TextButton onClick={(e) => { handleAccept(e, invite.roomId); }} disabled={isDecliningInvite || isAcceptingInvite}>
                         { isAcceptingInvite ? <LoadingSpinnerInline /> : <AcceptIcon /> }
                     </TextButton>
                 </ServiceTable.Cell>
-                <ServiceTable.Cell title={t('decline invitation')} onClick={(e) => {handleDecline(e, invite.roomId);}}>
-                    <TextButton disabled={isDecliningInvite || isAcceptingInvite}>
+                <ServiceTable.Cell title={t('decline invitation')}>
+                    <TextButton onClick={(e) => {handleDecline(e, invite.roomId);}} disabled={isDecliningInvite || isAcceptingInvite}>
                         { isDecliningInvite ? <LoadingSpinnerInline /> : <CloseIcon /> }
                     </TextButton>
                 </ServiceTable.Cell>
