a0f4a423b06ad71eaf06933835e6dfc9ea399dc8
Marcel <marcel-klasse@users.noreply.github.com>
2023-09-06 17:36:53 +0200

    adapt to new ServiceTable and add funtion to change power level of room members

--- pages/explore/UserManagement.js
+++ pages/explore/UserManagement.js
@@ -8,6 +8,7 @@ import BinIcon from '../../assets/icons/bin.svg';
 import TextButton from '../../components/UI/TextButton';
 import ErrorMessage from '../../components/UI/ErrorMessage';
 import LoadingSpinnerInline from '../../components/UI/LoadingSpinnerInline';
+import presets from './presets';
 
 const UserManagement = ({ roomId, roomName }) => {
     const [errorMessage, setErrorMessage] = useState('');
@@ -17,30 +18,65 @@ const UserManagement = ({ roomId, roomName }) => {
     // get the current members of the room and sort them first from highest to lowest power level and then alphabetically
     const currentMembers = _.orderBy(room.getMembers(), ['powerLevel', 'name'], ['desc', 'asc']);
     const selfObject = currentMembers.filter(member => member.userId === matrixClient.getUserId())[0];
+    const { t } = useTranslation();
 
-    const handleKick = async (userId) => {
-        await matrixClient.kick(roomId, userId)
-            .catch(error => setErrorMessage(error.data.error));
+    const handleKick = async (userId, name) => {
+        if (confirm(t('Are you sure you want to kick {{name}} from {{room}}', { name: name, room: roomName }))) {
+            await matrixClient.kick(roomId, userId)
+                .catch(error => setErrorMessage(error.data.error));
+        }
+    };
+
+    const changePowerLevel = async (userId, level, name) => {
+        console.log(level);
+        const label = presets.powerLevels.find(role => role.level === level).label;
+
+        if (confirm(t('Are you sure you want to promote {{name}} to {{role}}', { name: name, role: label }))) {
+            await matrixClient.setPowerLevel(roomId, userId, level)
+                .catch(error => setErrorMessage(error.data.error));
+        }
     };
 
     return (<>
         <ServiceTable>
-            { currentMembers.map(member => {
+            <ServiceTable.Caption>
+                { t('All members of') } { roomName }
+            </ServiceTable.Caption>
+            <ServiceTable.Head>
+                <ServiceTable.Row>
+                    <ServiceTable.Header align="left">
+                        { t('Name') }
+                    </ServiceTable.Header>
+                    <ServiceTable.Header align="left">
+                        { t('User ID') }
+                    </ServiceTable.Header>
+                    <ServiceTable.Header align="left">
+                        { t('Role') }
+                    </ServiceTable.Header>
+                    <ServiceTable.Header>
+                        { t('Kick') }
+                    </ServiceTable.Header>
+                </ServiceTable.Row>
+            </ServiceTable.Head>
+            <ServiceTable.Body>
+                { currentMembers.map(member => {
                 // users who once joined the room but left the room already are still listed in the members object
                 // therefore we need to filter them
-                if (member.membership === 'leave') return null;
-                // we don't want to display the currently logged in user
-                if (member.userId === selfObject.userId) return null;
+                    if (member.membership === 'leave') return null;
+                    // we don't want to display the currently logged in user
+                    if (member.userId === selfObject.userId) return null;
 
-                return <UserTableRow
-                    key={member.userId}
-                    displayName={member.name}
-                    userId={member.userId}
-                    roomName={roomName}
-                    powerLevel={member.powerLevel}
-                    selfPowerLevel={selfObject.powerLevel}
-                    handleKick={handleKick} />;
-            }) }
+                    return <UserTableRow
+                        key={member.userId}
+                        displayName={member.name}
+                        userId={member.userId}
+                        roomName={roomName}
+                        powerLevel={member.powerLevel}
+                        selfPowerLevel={selfObject.powerLevel}
+                        handleKick={handleKick}
+                        changePowerLevel={changePowerLevel} />;
+                }) }
+            </ServiceTable.Body>
         </ServiceTable>
         { errorMessage && <ErrorMessage>{ errorMessage }</ErrorMessage> }
     </>
@@ -48,20 +84,42 @@ const UserManagement = ({ roomId, roomName }) => {
 };
 export default UserManagement;
 
-function UserTableRow({ displayName, userId, roomName, powerLevel, selfPowerLevel, handleKick }) {
+function UserTableRow({ displayName, userId, roomName, powerLevel, selfPowerLevel, handleKick, changePowerLevel }) {
     const [isKicking, setIsKicking] = useState(false);
+    const [isChangingPowerLevel, setIsChangingPowerLevel] = useState(false);
     const { t } = useTranslation();
 
     const onKickClick = async (e) => {
         e.preventDefault();
         setIsKicking(true);
-        await handleKick(userId);
+        await handleKick(userId, displayName);
         setIsKicking(false);
     };
 
+    const onPowerLevelChange = async (e, level) => {
+        e.preventDefault();
+        setIsChangingPowerLevel(true);
+        await changePowerLevel(userId, parseInt(level), displayName);
+        setIsChangingPowerLevel(false);
+    };
+
     return <ServiceTable.Row>
-        <ServiceTable.Cell>
+        <ServiceTable.Cell title={t('Name')}>
             { displayName }
+        </ServiceTable.Cell>
+        <ServiceTable.Cell title={t('User Id')}>
+            { userId }
+        </ServiceTable.Cell>
+        <ServiceTable.Cell title={t('Role')}>
+            <select
+                defaultValue={powerLevel}
+                disabled={powerLevel >= selfPowerLevel || selfPowerLevel === powerLevel || isChangingPowerLevel}
+                onChange={(e) => onPowerLevelChange(e, e.target.value)}
+            >
+                { presets.powerLevels.map((role => {
+                    return <option key={role.level} value={role.level} disabled={role.level <= powerLevel || role.level > selfPowerLevel}>{ role.label }</option>;
+                })) }
+            </select>
         </ServiceTable.Cell>
         <ServiceTable.Cell>
             <TextButton
