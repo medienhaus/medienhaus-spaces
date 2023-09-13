118dbed9f3d0f85ee53dada0bd045eb3c9bc3060
Marcel <marcel-klasse@users.noreply.github.com>
2023-09-13 19:11:12 +0200

    disable custom styling

--- components/UI/Checkbox.js
+++ components/UI/Checkbox.js
@@ -7,29 +7,6 @@ const Wrapper = styled.div`
   align-items: center;
   justify-content: start;
 
-  input {
-    width: calc(var(--margin) * 2);
-    height: calc(var(--margin) * 2);
-    cursor: pointer;
-    border: unset;
-    border-color: var(--color-fg);
-    border-style: solid;
-    border-width: calc(var(--margin) * 0.2);
-    border-radius: unset;
-    box-shadow: none;
-    appearance: none;
-  }
-
-  &:checked {
-    font-size: 1.4rem;
-    color: var(--color-bg);
-    text-align: center;
-    background-color: var(--color-fg);
-  }
-
-  &:checked::after {
-    content: '\2713';
-  }
 `;
 
 const Checkbox = ({ children, isChecked, onClick }) => {
