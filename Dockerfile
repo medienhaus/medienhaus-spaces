medienhaus/medienhaus-spaces

commit fbda7e88ccd780a9002c7efac0885aca049c0dc5

Dockerfile RELOADED
- This improves our Dockerfile for both, development and production purposes.
- docker-compose.yml was slightly adjusted to showcase a development use-case.
- Added .dockerignore, taken from `docker init`.

- As a bonus the Docker image finally respects SIGTERM signals, which means it does not take 10 seconds for the container to stop.

Dockerfile
@@ -1,24 +1,77 @@
-# Taken from https://dev.to/deepakfilth/how-i-dockerized-my-next-js-website-4f3a
+# syntax=docker/dockerfile:1
+# Taken with a pinch of salt from https://github.com/vercel/next.js/blob/524b31513a58e58e15862ac8aa3f27da8a47a267/examples/with-docker-compose/next-app/prod.Dockerfile
 
-# Creates a layer from node:alpine image.
-FROM node:alpine
+FROM node:lts-alpine AS base
 
-# Sets an environment variable
-ENV PORT 3000
 
-# Sets the working directory for any RUN, CMD, ENTRYPOINT, COPY, and ADD commands
-WORKDIR /usr/src/app
+# Step 1. Install dependencies
+FROM base AS dependency-installer
 
-# Copy new files or directories into the filesystem of the container
-COPY package.json /usr/src/app
-COPY package-lock.json /usr/src/app
+WORKDIR /app
 
-# Execute commands in a new layer on top of the current image and commit the results
+# Install dependencies
+COPY package.json package-lock.json ./
 RUN npm install
 
+
+
+# Step 2. Development image
+FROM dependency-installer AS development-runner
+
+WORKDIR /app
+
+# Environment variables must be present at build time
+# https://github.com/vercel/next.js/discussions/14030
+ARG NODE_ENV
+ENV NODE_ENV=${NODE_ENV}
+ENV NEXT_TELEMETRY_DISABLED=1
+
+
 EXPOSE 3000
+# Taken from https://github.com/vercel/next.js/blob/d21025cc3a50e2ff8a7137d5d5c94576218f01e7/package.json#L50
+CMD ["node", "--trace-deprecation", "--enable-source-maps", "node_modules/.bin/next"]
 
-ENTRYPOINT ["npm", "run"]
 
-# Use this command as a default (can be overridden via "command" option in docker-compose.yml)
-CMD ["dev"]
+
+# Step 3. Build a production-optimized version of the app
+FROM dependency-installer AS production-builder
+
+WORKDIR /app
+
+# Environment variables must be redefined at run time
+ARG NODE_ENV
+ENV NODE_ENV=${NODE_ENV}
+
+
+COPY . .
+
+# Build Next.js app
+RUN npm run build
+
+
+
+# Step 4. Production image, copy all the files and run next
+FROM base AS production-runner
+
+WORKDIR /app
+
+# Environment variables must be redefined at run time
+ARG NODE_ENV
+ENV NODE_ENV=${NODE_ENV}
+ENV NEXT_TELEMETRY_DISABLED=1
+
+
+# Don't run production as root
+RUN addgroup --system --gid 1001 nodejs
+RUN adduser --system --uid 1001 nextjs
+USER nextjs
+
+COPY --from=production-builder /app/public ./public
+
+# Automatically leverage output traces to reduce image size
+# https://nextjs.org/docs/advanced-features/output-file-tracing
+COPY --from=production-builder --chown=nextjs:nodejs /app/.next/standalone ./
+COPY --from=production-builder --chown=nextjs:nodejs /app/.next/static ./.next/static
+
+EXPOSE 3000
+CMD ["node", "server.js"]

docker-compose.yml
@@ -21,14 +21,15 @@ services:
   spaces:
     build:
       context: .
+      target: "development-runner"
     labels:
       traefik.enable: "true"
       traefik.http.routers.spaces.entrypoints: "web"
       traefik.http.routers.spaces.rule: "PathPrefix(`/`)"
     volumes:
-      - .:/usr/src/app
-      - /usr/src/app/node_modules
-      - /usr/src/app/.next
+      - .:/app
+    environment:
+      - NODE_ENV=development
 
   element:
     image: vectorim/element-web:latest

.dockerignore
@@ -1,0 +1,34 @@
+# Include any files or directories that you don't want to be copied to your
+# container here (e.g., local build artifacts, temporary files, etc.).
+#
+# For more help, visit the .dockerignore file reference guide at
+# https://docs.docker.com/engine/reference/builder/#dockerignore-file
+
+**/.classpath
+**/.dockerignore
+**/.env
+**/.git
+**/.gitignore
+**/.project
+**/.settings
+**/.toolstarget
+**/.vs
+**/.vscode
+**/.next
+**/.cache
+**/*.*proj.user
+**/*.dbmdl
+**/*.jfm
+**/charts
+**/docker-compose*
+**/compose*
+**/Dockerfile*
+**/node_modules
+**/npm-debug.log
+**/obj
+**/secrets.dev.yaml
+**/values.dev.yaml
+**/build
+**/dist
+LICENSE
+README.md

next.config.example.js
@@ -58,5 +58,6 @@ module.exports = {
 
         return rewriteConfig;
     },
+    output: 'standalone',
     webpack: WebpackConfig,
 };

commit 110e072a020287c7fbbf4c19d2a0dde9431e381e

Fix Docker dev environment
Broke this in fbda7e88ccd780a9002c7efac0885aca049c0dc5

Dockerfile
@@ -11,7 +11,7 @@ WORKDIR /app
 
 # Install dependencies
 COPY package.json package-lock.json ./
-RUN npm install
+RUN npm clean-install
 
 
 

docker-compose.yml
@@ -28,6 +28,8 @@ services:
       traefik.http.routers.spaces.rule: "PathPrefix(`/`)"
     volumes:
       - .:/app
+      - medienhaus-spaces--node_modules:/app/node_modules
+      - medienhaus-spaces--.next:/app/.next
     environment:
       - NODE_ENV=development
 
@@ -42,3 +44,9 @@ services:
       traefik.http.middlewares.element.stripprefix.prefixes: "/element"
     volumes:
       - ./element.config.json:/app/config.json
+
+volumes:
+  medienhaus-spaces--node_modules:
+    driver: local
+  medienhaus-spaces--.next:
+    driver: local