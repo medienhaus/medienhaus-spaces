54c561032052eaf1f8828e9760667365008ddecf
Marcel <marcel-klasse@users.noreply.github.com>
2023-10-31 12:37:50 +0100

    reafactor: add individual disable props for confirm and cancel

--- components/UI/ConfirmCancelButtons.js
+++ components/UI/ConfirmCancelButtons.js
@@ -27,13 +27,13 @@ const CancelButton = styled.button`
   }
 `;
 
-const ConfirmCancelButtons = ({ children, disabled, onClick, onCancel }) => {
+const ConfirmCancelButtons = ({ children, disabled, disableConfirm, disableCancel, onClick, onCancel }) => {
     const { t } = useTranslation();
 
     return (
         <ConfirmCancelButtonsWrapper>
-            <ConfirmButton type="submit" disabled={disabled} onClick={onClick}>{ children ? children : t('Confirm') }</ConfirmButton>
-            <CancelButton type="reset" disabled={disabled} onClick={onCancel}>{ t('Cancel') }</CancelButton>
+            <ConfirmButton type="submit" disabled={disabled || disableConfirm} onClick={onClick}>{ children ? children : t('Confirm') }</ConfirmButton>
+            <CancelButton type="reset" disabled={disabled || disableCancel} onClick={onCancel}>{ t('Cancel') }</CancelButton>
         </ConfirmCancelButtonsWrapper>
     );
 };
