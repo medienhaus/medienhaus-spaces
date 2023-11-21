6bb5679cc57d37dae6c914867a9193dd5e372d52
Florian Weber <florian@weber.world>
2023-11-21 15:34:43 +0100

    Decrease <LoadingSpinnerInline> size

--- components/UI/LoadingSpinnerInline.js
+++ components/UI/LoadingSpinnerInline.js
@@ -4,8 +4,9 @@ import LoadingSpinner from './LoadingSpinner';
 
 const LoadingSpinnerInline = styled(LoadingSpinner)`
   display: inline-block;
-  width: calc(var(--margin) * 1.4);
-  height: calc(var(--margin) * 1.4);
+  width: calc(var(--margin) * 1.2);
+  height: calc(var(--margin) * 1.2);
+  border-width: calc(var(--margin) * 0.145);
 `;
 
 export default LoadingSpinnerInline;
