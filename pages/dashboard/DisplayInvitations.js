960549730a7a98f0405bc9dcfc5fdcbcd5d7d628
Marcel <marcel-klasse@users.noreply.github.com>
2023-11-21 14:13:01 +0100

    feat: add handled view and fix translations

--- pages/dashboard/DisplayInvitations.js
+++ pages/dashboard/DisplayInvitations.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useTranslation } from 'react-i18next';
+import { Trans, useTranslation } from 'react-i18next';
 import Link from 'next/link';
 import styled from 'styled-components';
 
@@ -38,7 +38,7 @@ const InvitationCard = styled.div`
   }
 `;
 
-export default function DisplayInvitations({ invite, path, service, acceptMatrixInvite, declineMatrixInvite }) {
+export default function DisplayInvitations({ invite, path, service, acceptMatrixInvite, declineMatrixInvite, membership }) {
     const { t } = useTranslation('dashboard');
     const [isAcceptingInvite, setIsAcceptingInvite] = useState(false);
     const [isDecliningInvite, setIsDecliningInvite] = useState(false);
@@ -73,9 +73,9 @@ export default function DisplayInvitations({ invite, path, service, acceptMatrix
 
         setLink(forwardingUrl);
         setWasHandled(true);
-
-        return forwardingUrl;
     };
+
+    if (wasHandled) return <HandledInvitation name={invite.name} link={link} />;
 
     return (
         <InvitationCard>
@@ -84,16 +84,44 @@ export default function DisplayInvitations({ invite, path, service, acceptMatrix
                     <Link href={link}>{ invite.name }</Link>
                     : invite.name }
             </h4>
-            <p title={invite.inviter?.userId}>
-                { invite.inviter?.displayName }<em>  invited you to join this   </em>{ path } <em>item</em>
+            <p>
+                <Trans
+                    t={t}
+                    i18nKey="invitationCard"
+                    defaults="<italic>{{name}}</italic> invited you to join this <bold><italic>{{service}}</italic></bold> item"
+                    values={{ name: invite.inviter?.displayName, service: path }}
+                    components={{ italic: <em />, bold: <strong /> }}
+                />
+
             </p>
             <ConfirmCancelButtons
-                onClick={(e) => {handleAccept(e, invite.roomId); }}
+                onClick={(e) => { handleAccept(e, invite.roomId); }}
                 onCancel={(e) => handleDecline(e, invite.roomId)}
                 disabled={isDecliningInvite || isAcceptingInvite || wasHandled}
-                cancelLabel={t('reject')}
-                confirmLabel={t('accept')}
+                cancelLabel={t('Decline')}
+                confirmLabel={t('Accept')}
             />
         </InvitationCard>
     );
 }
+
+const HandledInvitation = ({ name, link }) => {
+    const { t } = useTranslation('dashboard');
+
+    return (
+        <InvitationCard>
+            <h4>
+                { name }
+            </h4>
+            { link ? <p>
+                <Trans
+                    t={t}
+                    i18nKey="invitationCardHandled"
+                >
+                    You’ve successfully joined <Link href={link}><strong>{ { name } }</strong></Link></Trans>
+            </p>
+                : <p> { t('You’ve declined the invitation') } </p>
+            }
+        </InvitationCard>
+    );
+};
