c01820d09530261f9c3465633fdaaab27948f25f
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-11-30 13:14:15 +0100

    style: refactor ServiceLink for use in `/chat` route

--- components/UI/ServiceLink.js
+++ components/UI/ServiceLink.js
@@ -18,11 +18,40 @@ const LockIconWrapper = styled(Icon)`
   }
 `;
 
+const StyledServiceTableRow = styled(ServiceTable.Row)`
+  display: grid;
+
+  /* NOTE: if we _do_not_ display avatar, then ... */
+
+  grid-template-columns: max-content 1fr max-content;
+
+  /* NOTE: if we display avatar, then ... */
+
+  /*
+  grid-template-columns: max-content 1fr max-content;
+  */
+
+  align-items: center;
+
+  > :last-child:not(:only-child) {
+    justify-self: end;
+  }
+`;
+
+const ServiceTableLink = styled(Link)`
+  display: block;
+`;
+
+const SelectedMarker = styled.span`
+  /*
+  float: right;
+  */
+`;
+
 const NotificationBadge = styled.span`
-  display: inline-grid;
-  place-content: center;
-  height: calc(1rem * var(--line-height));
+  justify-self: end;
   padding: 0 calc(var(--margin) / 4);
+  line-height: var(--line-height);
   color: rgb(255 255 255);
   background-color: var(--color-notification);
 
@@ -31,7 +60,6 @@ const NotificationBadge = styled.span`
   }
 `;
 
-/*
 const Avatar = styled.img`
   position: relative;
   float: left;
@@ -43,37 +71,44 @@ const Avatar = styled.img`
     backdrop-filter: invert(100%);
   }
 `;
-*/
 
 const ServiceLink = forwardRef(({ name, href, selected, passwordProtected, notifications, avatar }, ref) => {
     const { t } = useTranslation();
 
     return (
-        <ServiceTable.Row>
-            { /*{ avatar && (*/ }
-            { /*    <ServiceTable.Cell>*/ }
-            { /*        <Avatar src={avatar} alt={name} />*/ }
-            { /*    </ServiceTable.Cell>) }*/ }
+        <StyledServiceTableRow>
+            { avatar && (
+                <ServiceTable.Cell>
+                    <Avatar src={avatar} alt={name} />
+                </ServiceTable.Cell>)
+            }
             <ServiceTable.Cell>
-                { /* Tell if this is our active item by displaying an arrow */ }
-                { selected && <span style={{ float: 'right' }}>→</span> }
-                <Link
-                    style={{ display: 'block' }}
+                <ServiceTableLink
                     ref={ref}
                     href={href}
                 >
                     { name }
-                    { /* Show notification badge if there are notifications */ }
-                    { notifications && <NotificationBadge>
+                    { /* Show a lock icon if this Link is password protected */ }
+                    { passwordProtected && <LockIconWrapper title={t('password protected')}><LockIcon /></LockIconWrapper> }
+                </ServiceTableLink>
+            </ServiceTable.Cell>
+            { /* Tell if this is our active item by displaying an arrow */ }
+            { selected &&
+                <ServiceTable.Cell>
+                    <SelectedMarker>→</SelectedMarker>
+                </ServiceTable.Cell>
+            }
+            { /* Show notification badge if there are notifications */ }
+            { !selected && notifications &&
+                <ServiceTable.Cell>
+                    <NotificationBadge>
                         <small>
                             { notifications < 100 ? notifications : '99+' }
                         </small>
-                    </NotificationBadge> }
-                    { /* Show a lock icon if this Link is password protected */ }
-                    { passwordProtected && <LockIconWrapper title={t('password protected')}><LockIcon /></LockIconWrapper> }
-                </Link>
-            </ServiceTable.Cell>
-        </ServiceTable.Row>
+                    </NotificationBadge>
+                </ServiceTable.Cell>
+            }
+        </StyledServiceTableRow>
     );
 });
 ServiceLink.displayName = 'ServiceLink';
