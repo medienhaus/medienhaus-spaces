3a636d4cc176594ab7614f10035fd535003aaab4
Florian Weber <florian@weber.world>
2023-11-30 13:59:12 +0100

    /dashboard is our default page!

--- pages/explore.js
+++ pages/explore.js
@@ -66,7 +66,7 @@ export default function Explore() {
     // If there's no context root space set in our configuration we can't make any use of the /explore page at
     // the moment, so we forward the user to `/`
     if (!getConfig().publicRuntimeConfig.contextRootSpaceRoomId) {
-        router.replace('/');
+        router.replace('/dashboard');
 
         return null;
     }
