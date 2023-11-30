1de593ab298c134c096ce045f07c5b3152ab42d0
Marcel <79279756+aofn@users.noreply.github.com>
2023-11-30 16:04:51 +0100

    wip: add placeholder

--- pages/dashboard/index.js
+++ pages/dashboard/index.js
@@ -106,6 +106,7 @@ export default function Dashboard() {
                     }) }
                 </>
             }
+            { /* show pending knocks here */ }
         </DefaultLayout.LameColumn>
     );
 }
