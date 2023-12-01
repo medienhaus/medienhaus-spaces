6b15812e85464e60760792ba5770a2baafa2e5dd
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-12-01 15:27:32 +0100

    feat: introduce `<ButtonInlineIcon />` styled component

---
+++ components/UI/ButtonInlineIcon.js
@@ -0,0 +1,35 @@
+import styled from 'styled-components';
+
+import Button from './Button';
+
+const ButtonInlineIcon = styled(Button)`
+  display: inline-grid;
+  color: var(--color-foreground);
+  background-color: var(--color-background);
+  border-color: transparent;
+
+  svg {
+    fill: var(--color-foreground) !important;
+  }
+
+  /*
+  &:disabled {
+    svg {
+      fill: var(--color-disabled) !important;
+    }
+  }
+  */
+
+  &:not(:disabled):hover {
+    svg {
+      fill: var(--color-notification) !important;
+      transition: all 200ms ease;
+    }
+  }
+
+  &:not(:disabled):focus {
+    border-color: var(--color-notification);
+  }
+`;
+
+export default ButtonInlineIcon;
