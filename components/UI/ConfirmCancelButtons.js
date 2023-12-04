9c126959bd13f8e26bc07fbd8445ee80e8ea0717
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-12-04 11:57:06 +0100

    feat: use new Button… components in ConfirmCancelButtons

--- components/UI/ConfirmCancelButtons.js
+++ components/UI/ConfirmCancelButtons.js
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
+import ButtonPrimary from './ButtonPrimary';
+import ButtonSecondary from './ButtonSecondary';
+
 const ConfirmCancelButtonsWrapper = styled.div`
   display: grid;
   grid-template-columns: repeat(2, 1fr);
@@ -22,40 +25,6 @@ const SmallConfirmCancelButtonsWrapper = styled(ConfirmCancelButtonsWrapper)`
   }
 `;
 
-const ConfirmButton = styled.button`
-  color: var(--color-background);
-  background-color: var(--color-foreground);
-
-  &:disabled {
-    color: var(--color-background);
-    background-color: var(--color-disabled);
-  }
-
-  &:hover {
-    color: rgb(255 255 255);
-    background-color: var(--color-notification);
-    border-color: var(--color-notification);
-    transition: all 200ms ease;
-  }
-`;
-
-const CancelButton = styled.button`
-  color: var(--color-foreground);
-  background-color: var(--color-background);
-
-  &:disabled {
-    color: var(--color-disabled);
-    background-color: var(--color-background);
-  }
-
-  &:hover {
-    color: var(--color-notification);
-    background-color: var(--color-background);
-    border-color: var(--color-notification);
-    transition: all 200ms ease;
-  }
-`;
-
 const ConfirmCancelButtons = ({ disabled, confirmLabel, cancelLabel, small }) => {
     const { t } = useTranslation();
 
@@ -66,8 +35,8 @@ const ConfirmCancelButtons = ({ disabled, confirmLabel, cancelLabel, small }) =>
 
     return (
         <Wrapper>
-            <CancelButton type="reset" disabled={disabled}>{ cancelLabel || t('Cancel') }</CancelButton>
-            <ConfirmButton type="submit" disabled={disabled}>{ confirmLabel || t('Confirm') }</ConfirmButton>
+            <ButtonSecondary type="reset" disabled={disabled}>{ cancelLabel || t('Cancel') }</ButtonSecondary>
+            <ButtonPrimary type="submit" disabled={disabled}>{ confirmLabel || t('Confirm') }</ButtonPrimary>
         </Wrapper>
     );
 };
