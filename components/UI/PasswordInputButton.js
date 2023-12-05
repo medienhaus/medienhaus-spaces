6a84f5541d00bb65d43c96a5a983f9df7c1b2b9a
Andi Rueckel <andirueckel@users.noreply.github.com>
2023-12-05 11:52:32 +0100

    chore: use Input in PasswordInputButton

--- components/UI/PasswordInputButton.js
+++ components/UI/PasswordInputButton.js
@@ -2,36 +2,20 @@ import styled from 'styled-components';
 import { useTranslation } from 'react-i18next';
 
 import ButtonPrimary from './ButtonPrimary';
+import Input from './Input';
 
 const PasswordInputButtonContainer = styled.div`
   display: grid;
   grid-template-columns: 1fr min-content;
   grid-gap: 0 calc(var(--margin) / var(--line-height));
   align-items: start;
-
-  /* make button child element the same height as input child element *
-   * NOTE: input height is styled globally via /assets/_globalCss.css */
-  > * {
-    height: 100%;
-  }
-
-  /* NOTE: once input elements are no longer styled globally, we could *
-   * set the container height, and change the selector as done below … *
-
-  height: calc(1rem * var(--line-height) * 4);
-
-  > input,
-  > button {
-    height: 100%;
-  }
-  */
 `;
 
 export default function PasswordInputButton({ placeholder, value, onChange, disabled, label }) {
     const { t } = useTranslation();
 
     return <PasswordInputButtonContainer>
-        <input type="password"
+        <Input type="password"
             placeholder={placeholder}
             value={value}
             onChange={onChange}
