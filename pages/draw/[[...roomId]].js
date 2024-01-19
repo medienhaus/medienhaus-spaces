eb4b433e2db61e1e8353d89fb1d23dbeb970b1d8
Florian Weber <florian@weber.world>
2024-01-19 16:22:48 +0100

    The template should === the name of the service provider, which is `tldraw` in this case

--- pages/draw/[[...roomId]].js
+++ pages/draw/[[...roomId]].js
@@ -89,7 +89,7 @@ export default function Draw() {
     const createSketchRoom = useCallback(async (name) => {
         if (!name) return;
 
-        const newRoom = await matrix.createRoom(name, false, '', 'public', 'item', 'tldraw-sketch', serviceSpaceId);
+        const newRoom = await matrix.createRoom(name, false, '', 'public', 'item', 'tldraw', serviceSpaceId);
 
         // we add the defualt store schama data and the store data as an custom stateevent to the newly created room
         await matrixClient.sendStateEvent(newRoom, 'dev.medienhaus.tldraw.store.schema', JSON.stringify({ 'schemaVersion': 1, 'storeVersion': 4, 'recordVersions': { 'asset': { 'version': 1, 'subTypeKey': 'type', 'subTypeVersions': { 'image': 2, 'video': 2, 'bookmark': 0 } }, 'camera': { 'version': 1 }, 'document': { 'version': 2 }, 'instance': { 'version': 22 }, 'instance_page_state': { 'version': 5 }, 'page': { 'version': 1 }, 'shape': { 'version': 3, 'subTypeKey': 'type', 'subTypeVersions': { 'group': 0, 'text': 1, 'bookmark': 1, 'draw': 1, 'geo': 7, 'note': 4, 'line': 1, 'frame': 0, 'arrow': 2, 'highlight': 0, 'embed': 4, 'image': 2, 'video': 1 } }, 'instance_presence': { 'version': 5 }, 'pointer': { 'version': 1 } } }));
