55858aca3709ea49c1fc177fedaab05a720556c7
Marcel <79279756+aofn@users.noreply.github.com>
2024-01-23 17:50:42 +0100

    refactor: move eslint config into eslint config file

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,4 +17,57 @@ module.exports = {
             },
         },
     },
+    plugins: [
+        'matrix-org',
+    ],
+    extends: [
+        'next',
+        'plugin:matrix-org/javascript',
+        'plugin:matrix-org/a11y',
+        'plugin:matrix-org/react',
+        'plugin:import/recommended',
+    ],
+    env: {
+        'es6': true,
+    },
+    rules: {
+        'padding-line-between-statements': [
+            'error',
+            {
+                'blankLine': 'always',
+                'prev': '*',
+                'next': 'return',
+            },
+            {
+                'blankLine': 'always',
+                'prev': '*',
+                'next': 'export',
+            },
+            {
+                'blankLine': 'always',
+                'prev': '*',
+                'next': 'multiline-block-like',
+            },
+            {
+                'blankLine': 'always',
+                'prev': 'multiline-block-like',
+                'next': '*',
+            },
+        ],
+        'max-len': 'off',
+        'jsx-quotes': [
+            'error',
+            'prefer-double',
+        ],
+    },
+    settings: {
+        'import/resolver': {
+            'alias': {
+                'map': [
+                    ['@', './'],
+                ],
+                'extensions': ['.js', '.jsx', '.ts', '.tsx'],
+            },
+        },
+    },
 };
