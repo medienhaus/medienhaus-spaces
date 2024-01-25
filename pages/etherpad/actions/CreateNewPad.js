0ef6e4c101afffde2644d313efacb1c8d3c5ce46
Marcel <79279756+aofn@users.noreply.github.com>
2024-01-25 13:49:07 +0100

    refactor: createPad function to create private or public password depending on selection. Add headline to component

--- pages/etherpad/actions/CreateNewPad.js
+++ pages/etherpad/actions/CreateNewPad.js
@@ -5,8 +5,8 @@ import { styled } from 'styled-components';
 import Form from '../../../components/UI/Form';
 import LoadingSpinnerInline from '../../../components/UI/LoadingSpinnerInline';
 import logger from '../../../lib/Logging';
-
 import { Switch } from '@/components/UI/switch.jsx';
+import ErrorMessage from '@/components/UI/ErrorMessage';
 
 const SwitchContainer = styled.div`
   display: flex;
@@ -23,18 +23,24 @@ export default function CreateNewPad({ createPadAndOpen, isMyPadsApiEnabled, cal
     const [password, setPassword] = useState('');
     const [validatePassword, setValidatePassword] = useState('');
     const [isLoading, setIsLoading] = useState(false);
+    const [errorMessage, setErrorMessage] = useState('');
 
-    const createPasswordPad = async () => {
+    const createPad = async () => {
         setIsLoading(true);
-        const createPad = await createPadAndOpen(padName, 'private', password)
-            .catch(error => logger.debug(error));
+        setErrorMessage('');
+        const createPad = await createPadAndOpen(padName, passwordProtected ? 'private' : 'public', password)
+            .catch(error => {
+                logger.debug(error);
+                setErrorMessage('Something went wrong, please try again.');
+            });
         setIsLoading(false);
         if (!createPad) return;
         callbackDone && callbackDone();
         setPadName('');
     };
 
-    return (<Form onSubmit={(e) => { e.preventDefault(); createPasswordPad(); }}>
+    return (<Form onSubmit={(e) => { e.preventDefault(); createPad(); }}>
+        <h3>{ t('Create new pad') }</h3>
         <input type="text" placeholder={t('Name')} value={padName} onChange={(e) => setPadName(e.target.value)} />
         { isMyPadsApiEnabled &&
       <SwitchContainer>
@@ -49,5 +55,6 @@ export default function CreateNewPad({ createPadAndOpen, isMyPadsApiEnabled, cal
         <input type="password" disabled={!passwordProtected} placeholder={t('Password')} value={password} onChange={(e) => setPassword(e.target.value)} />
         <input type="password" disabled={!passwordProtected} placeholder={t('Confirm password')} value={validatePassword} onChange={(e) => setValidatePassword(e.target.value)} />
         <button type="submit" disabled={!padName || (passwordProtected && !password || password !== validatePassword)}>{ isLoading ? <LoadingSpinnerInline inverted /> : t('Create pad') }</button>
+        { errorMessage && <ErrorMessage>{ errorMessage }</ErrorMessage> }
     </Form>);
 }
