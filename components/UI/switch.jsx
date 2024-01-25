e4d5fd9df64f2ce0231a061684b906dae12b859b
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-01-25 12:17:22 +0100

    fix: remove inline-flex and padding from switch

--- components/UI/switch.jsx
+++ components/UI/switch.jsx
@@ -6,7 +6,7 @@ import { cn } from '@/lib/utils';
 const Switch = React.forwardRef(({ className, ...props }, ref) => (
     <SwitchPrimitives.Root
         className={cn(
-            'tw-peer tw-inline-flex tw-h-6 tw-w-11 tw-shrink-0 tw-cursor-pointer tw-items-center tw-rounded-full tw-border-2 tw-border-transparent tw-transition-colors focus-visible:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-ring focus-visible:tw-ring-offset-2 focus-visible:tw-ring-offset-background disabled:tw-cursor-not-allowed disabled:tw-opacity-50 data-[state=checked]:tw-bg-primary data-[state=unchecked]:tw-bg-input',
+            'tw-p-0 tw-peer tw-h-6 tw-w-11 tw-shrink-0 tw-cursor-pointer tw-items-center tw-rounded-full tw-border-2 tw-border-transparent tw-transition-colors focus-visible:tw-outline-none focus-visible:tw-ring-2 focus-visible:tw-ring-ring focus-visible:tw-ring-offset-2 focus-visible:tw-ring-offset-background disabled:tw-cursor-not-allowed disabled:tw-opacity-50 data-[state=checked]:tw-bg-primary data-[state=unchecked]:tw-bg-input',
             className,
         )}
         {...props}
