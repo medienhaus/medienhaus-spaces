19c944cf85701fcfce8fa2383c304556ea03d8b1
Marcel <79279756+aofn@users.noreply.github.com>
2024-01-31 13:22:35 +0100

    remove container from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,13 +13,13 @@ module.exports = {
     // },
     prefix: '',
     theme: {
-        container: {
-            center: true,
-            padding: '2rem',
-            screens: {
-                '2xl': '1400px',
-            },
-        },
+        // container: {
+        //     center: true,
+        //     padding: '2rem',
+        //     screens: {
+        //         '2xl': '1400px',
+        //     },
+        // },
         extend: {
             colors: {
                 border: 'hsl(var(--border))',
