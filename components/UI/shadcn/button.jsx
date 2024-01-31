beb145dbda1d9d6325341e10ffbfc9d6426eac53
Marcel <79279756+aofn@users.noreply.github.com>
2024-01-31 14:47:14 +0100

    refactor: change default button hover to bg-accent

--- components/UI/shadcn/button.jsx
+++ components/UI/shadcn/button.jsx
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         variants: {
             variant: {
                 default:
-                    'bg-primary text-primary-foreground hover:bg-primary/90',
+                    'bg-primary text-primary-foreground hover:bg-accent hover:text-white',
                 destructive:
                     'bg-destructive text-destructive-foreground hover:bg-destructive/90',
                 outline:
