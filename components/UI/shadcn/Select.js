004b249ede4e534e2b13f6969280c4a30c198a85
Florian Weber <florian@weber.world>
2024-02-02 15:50:13 +0100

    Replace Lucide icons from shadcn/ui with our Remix icons

--- components/UI/shadcn/Select.js
+++ components/UI/shadcn/Select.js
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import * as SelectPrimitive from '@radix-ui/react-select';
-import { Check, ChevronDown, ChevronUp } from 'lucide-react';
+import { RiArrowDownSLine, RiArrowUpSLine, RiCheckLine } from '@remixicon/react';
 
 import { cn } from '@/lib/utils';
 
@@ -21,7 +21,7 @@ const SelectTrigger = React.forwardRef(({ className, children, ...props }, ref) 
     >
         {children}
         <SelectPrimitive.Icon asChild>
-            <ChevronDown className="h-4 w-4 opacity-50" />
+            <RiArrowDownSLine className="h-4 w-4 opacity-50" />
         </SelectPrimitive.Icon>
     </SelectPrimitive.Trigger>
 ));
@@ -29,7 +29,7 @@ SelectTrigger.displayName = SelectPrimitive.Trigger.displayName;
 
 const SelectScrollUpButton = React.forwardRef(({ className, ...props }, ref) => (
     <SelectPrimitive.ScrollUpButton ref={ref} className={cn('flex cursor-default items-center justify-center py-1', className)} {...props}>
-        <ChevronUp className="h-4 w-4" />
+        <RiArrowUpSLine className="h-4 w-4" />
     </SelectPrimitive.ScrollUpButton>
 ));
 SelectScrollUpButton.displayName = SelectPrimitive.ScrollUpButton.displayName;
@@ -40,7 +40,7 @@ const SelectScrollDownButton = React.forwardRef(({ className, ...props }, ref) =
         className={cn('flex cursor-default items-center justify-center py-1', className)}
         {...props}
     >
-        <ChevronDown className="h-4 w-4" />
+        <RiArrowDownSLine className="h-4 w-4" />
     </SelectPrimitive.ScrollDownButton>
 ));
 SelectScrollDownButton.displayName = SelectPrimitive.ScrollDownButton.displayName;
@@ -89,7 +89,7 @@ const SelectItem = React.forwardRef(({ className, children, ...props }, ref) => 
     >
         <span className="absolute left-2 flex h-3.5 w-3.5 items-center justify-center">
             <SelectPrimitive.ItemIndicator>
-                <Check className="h-4 w-4" />
+                <RiCheckLine className="h-4 w-4" />
             </SelectPrimitive.ItemIndicator>
         </span>
 
