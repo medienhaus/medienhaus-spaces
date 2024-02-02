785f85eb4d8c5f5826681ec6361a1c9a58886f27
Florian Weber <florian@weber.world>
2024-02-02 15:55:01 +0100

    Remove unnecessary wrapping element

--- pages/dashboard/InvitationCard.js
+++ pages/dashboard/InvitationCard.js
@@ -61,78 +61,76 @@ export default function InvitationCard({ roomId, roomName, inviterUsername, avat
     };
 
     return (
-        <>
-            <form onSubmit={handleAccept} onReset={handleDecline}>
-                <Card>
-                    <CardHeader>
-                        <CardTitle className="flex items-center gap-4">
-                            <Avatar>
-                                <AvatarImage src={avatar} />
-                                <AvatarFallback />
-                            </Avatar>
-                            {
-                                // Invites for direct messages
-                                isDm ? (
-                                    t('Direct Message')
-                                ) : // Application service specific invites (e.g. for Spacedeck, Etherpad, ...)
-                                service ? (
-                                    roomName
-                                ) : (
-                                    // All other invitations
-                                    <>
-                                        {roomName}
-                                        {joinRule === 'private' && (
-                                            <>
-                                                &nbsp;<em>({t('private')})</em>
-                                            </>
-                                        )}
-                                    </>
-                                )
-                            }
-                        </CardTitle>
-                    </CardHeader>
-                    <CardContent>
-                        {' '}
-                        {wasHandled ? (
-                            link ? (
-                                // Invitation accepted
-                                <Trans t={t} i18nKey="invitationCardHandled" values={{ roomName: roomName }}>
-                                    You can now view{' '}
-                                    <Link href={link}>
-                                        <strong>{roomName}</strong>
-                                    </Link>
-                                    .
-                                </Trans>
+        <form onSubmit={handleAccept} onReset={handleDecline}>
+            <Card>
+                <CardHeader>
+                    <CardTitle className="flex items-center gap-4">
+                        <Avatar>
+                            <AvatarImage src={avatar} />
+                            <AvatarFallback />
+                        </Avatar>
+                        {
+                            // Invites for direct messages
+                            isDm ? (
+                                t('Direct Message')
+                            ) : // Application service specific invites (e.g. for Spacedeck, Etherpad, ...)
+                            service ? (
+                                roomName
                             ) : (
-                                // Invitation rejected
-                                t('You’ve declined the invitation.')
+                                // All other invitations
+                                <>
+                                    {roomName}
+                                    {joinRule === 'private' && (
+                                        <>
+                                            &nbsp;<em>({t('private')})</em>
+                                        </>
+                                    )}
+                                </>
                             )
+                        }
+                    </CardTitle>
+                </CardHeader>
+                <CardContent>
+                    {' '}
+                    {wasHandled ? (
+                        link ? (
+                            // Invitation accepted
+                            <Trans t={t} i18nKey="invitationCardHandled" values={{ roomName: roomName }}>
+                                You can now view{' '}
+                                <Link href={link}>
+                                    <strong>{roomName}</strong>
+                                </Link>
+                                .
+                            </Trans>
                         ) : (
-                            // Invitation pending
-                            <Trans
-                                t={t}
-                                i18nKey="invitationCard"
-                                defaults="<bold>{{ username }}</bold> wants to <bold>{{ service }}</bold> with you."
-                                values={{ username: inviterUsername, service: path }}
-                                components={{ bold: <strong /> }}
+                            // Invitation rejected
+                            t('You’ve declined the invitation.')
+                        )
+                    ) : (
+                        // Invitation pending
+                        <Trans
+                            t={t}
+                            i18nKey="invitationCard"
+                            defaults="<bold>{{ username }}</bold> wants to <bold>{{ service }}</bold> with you."
+                            values={{ username: inviterUsername, service: path }}
+                            components={{ bold: <strong /> }}
+                        />
+                    )}
+                </CardContent>
+                <CardFooter>
+                    {!wasHandled && (
+                        <>
+                            <br />
+                            <ConfirmCancelButtons
+                                small
+                                disabled={isDecliningInvite || isAcceptingInvite || wasHandled}
+                                cancelLabel={t('Decline')}
+                                confirmLabel={t('Accept')}
                             />
-                        )}
-                    </CardContent>
-                    <CardFooter>
-                        {!wasHandled && (
-                            <>
-                                <br />
-                                <ConfirmCancelButtons
-                                    small
-                                    disabled={isDecliningInvite || isAcceptingInvite || wasHandled}
-                                    cancelLabel={t('Decline')}
-                                    confirmLabel={t('Accept')}
-                                />
-                            </>
-                        )}
-                    </CardFooter>
-                </Card>
-            </form>
-        </>
+                        </>
+                    )}
+                </CardFooter>
+            </Card>
+        </form>
     );
 }
