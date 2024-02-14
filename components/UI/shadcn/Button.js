e48797e5f7641b11ba8678abbb955bf554da92ce
Marcel <79279756+aofn@users.noreply.github.com>
2024-02-14 13:18:57 +0100

    refactor: use correct colour for destructive buttons

--- components/UI/shadcn/Button.js
+++ components/UI/shadcn/Button.js
@@ -10,7 +10,7 @@ const buttonVariants = cva(
         variants: {
             variant: {
                 default: 'bg-primary text-primary-foreground hover:bg-accent hover:text-white',
-                destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
+                destructive: 'bg-destructive text-white hover:bg-destructive/90',
                 outline: 'border border-input bg-background hover:bg-accent hover:border-accent hover:text-white',
                 secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
                 ghost: 'text-muted-foreground hover:text-foreground',
