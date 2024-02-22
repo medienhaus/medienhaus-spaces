c778ac0eabf67176f8b97f0fbfa727958fc0186b
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-02-22 15:29:17 +0100

    chore: remove unused mappings from tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,17 +2,7 @@
 module.exports = {
     darkMode: ['media'],
     content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
-    prefix: '',
     theme: {
-        fontSize: {
-            'sm': '1rem',
-            'base': '1rem',
-            'xl': '1rem',
-            '2xl': '1rem',
-            '3xl': '1rem',
-            '4xl': '1rem',
-            '5xl': '1rem',
-        },
         extend: {
             colors: {
                 border: 'hsl(var(--border))',
@@ -48,25 +38,6 @@ module.exports = {
                     DEFAULT: 'hsl(var(--card))',
                     foreground: 'hsl(var(--card-foreground))',
                 },
-            },
-            borderRadius: {
-                lg: 'var(--border-radius)',
-                md: 'var(--border-radius)',
-                sm: 'var(--border-radius)',
-            },
-            keyframes: {
-                'accordion-down': {
-                    from: { height: '0' },
-                    to: { height: 'var(--radix-accordion-content-height)' },
-                },
-                'accordion-up': {
-                    from: { height: 'var(--radix-accordion-content-height)' },
-                    to: { height: '0' },
-                },
-            },
-            animation: {
-                'accordion-down': 'accordion-down 0.2s ease-out',
-                'accordion-up': 'accordion-up 0.2s ease-out',
             },
         },
     },
