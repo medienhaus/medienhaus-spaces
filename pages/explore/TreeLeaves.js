d5644308029f41f60d4a4e8c8295903abbb0c7aa
Marcel <79279756+aofn@users.noreply.github.com>
2024-02-27 12:30:59 +0100

    refactor: create separate getIcon method to make it easier to add and change existing icons. check if a pad is password protected and display password icon if so

--- pages/explore/TreeLeaves.js
+++ pages/explore/TreeLeaves.js
@@ -1,33 +1,70 @@
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import getConfig from 'next/config';
 
 import LoadingSpinner from '../../components/UI/LoadingSpinner';
 import ServiceLink from '../../components/UI/ServiceLink';
+import { useAuth } from '@/lib/Auth';
+import { useMatrix } from '@/lib/Matrix';
+
+const getIcon = (template, name) => {
+    switch (template) {
+        case 'etherpad':
+            return '📝 ' + name;
+        case 'spacedeck':
+            return '🎨 ' + name;
+        case 'studentproject':
+            return '🎓 ' + name;
+        default:
+            return name;
+    }
+};
 
 const TreeLeaves = ({ leaf, selectedRoomId, isFetchingContent, small, isChat }) => {
     const router = useRouter();
+    const auth = useAuth();
+    const matrix = useMatrix();
+
+    const etherpad = auth.getAuthenticationProvider('etherpad');
+    const [isPasswordProtected, setIsPasswordProtected] = useState(false);
     if (!leaf) return <LoadingSpinner />;
 
     const roomId = leaf.id || leaf.room_id || leaf.roomId;
     const parentId = leaf.parent.id || leaf.parent.room_id || leaf.parent.roomId;
     const template = leaf.meta?.template;
-    // if the room is a chat or service we want to show a different icon
-    const name = isChat ? '💬 ' + leaf.name
-        : template === 'etherpad' ? '📝 ' + leaf.name
-            : template === 'spacedeck' ? '🎨 ' + leaf.name
-                : template === 'studentproject' ? '🎓 ' + leaf.name : leaf.name;
+    const name = isChat ? '💬 ' + leaf.name : getIcon(template, leaf.name);
+
     // if an iframe is open we only want to show items in the list
     if (selectedRoomId && leaf.type !== 'item') return null;
+
+    useEffect(() => {
+        let cancelled = false;
+        if (template === 'etherpad' && !cancelled) {
+            const checkIfPadHasPassword = async () => {
+                const url = matrix.roomContents.get(roomId)?.body;
+                const padId = url.split('/').pop();
+                return etherpad.isPadPasswordProtected(padId);
+            };
+
+            checkIfPadHasPassword().then((isProtected) => setIsPasswordProtected(isProtected));
+        }
+
+        return () => (cancelled = true);
+    }, []);
 
     return (
         <ServiceLink
             small={small}
             roomId={roomId}
-            href={getConfig().publicRuntimeConfig.templates?.item.includes(template) || isChat ? `/explore/${parentId}/${roomId}` : `/explore/${roomId}`}
+            href={
+                getConfig().publicRuntimeConfig.templates?.item.includes(template) || isChat
+                    ? `/explore/${parentId}/${roomId}`
+                    : `/explore/${roomId}`
+            }
             name={name}
             isFetchingContent={isFetchingContent}
             selected={router.query.roomId[1] === roomId || router.query.roomId[0] === roomId}
+            passwordProtected={isPasswordProtected}
         />
     );
 };
