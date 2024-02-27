9c8a93240f164276846bc8e98a5f71ef91342a43
Marcel <79279756+aofn@users.noreply.github.com>
2024-02-27 12:29:40 +0100

    refactor: add mypads auth token to url so authors of the the pad don't have to input the password again

--- pages/explore/ExploreIframeViews.js
+++ pages/explore/ExploreIframeViews.js
@@ -4,36 +4,42 @@ import React, { useEffect, useState } from 'react';
 import ServiceIframeHeader from '../../components/UI/ServiceIframeHeader';
 import ChatIframeView from '../chat/ChatIframeView';
 import ProjectView from './ProjectView';
-import { useAuth } from '../../lib/Auth';
-import { useMatrix } from '../../lib/Matrix';
+import { useAuth } from '@/lib/Auth';
+import { useMatrix } from '@/lib/Matrix';
 import DefaultLayout from '../../components/layouts/default';
 
 const ExploreIframeViews = ({ currentTemplate, iframeRoomId, title: parsedTitle }) => {
     const auth = useAuth();
     const matrix = useMatrix(auth.getAuthenticationProvider('matrix'));
     const matrixClient = auth.getAuthenticationProvider('matrix').getMatrixClient();
+    const etherpad = auth.getAuthenticationProvider('etherpad');
+
     const [title, setTitle] = useState(parsedTitle);
+
+    const iframeUrl = currentTemplate === 'studentProject' ? iframeRoomId : new URL(matrix.roomContents.get(iframeRoomId)?.body);
+
+    // add auth token to etherpad iframe, so authors of the pad don't have to input the password again
+    if (currentTemplate === 'etherpad') {
+        iframeUrl.searchParams.set('auth_token', etherpad.getToken());
+    }
 
     useEffect(() => {
         let cancelled = false;
 
         const fetchRoomName = async () => {
-            const nameEvent = await matrixClient.getStateEvent(
-                iframeRoomId,
-                'm.room.name',
-            ).catch(() => {});
+            const nameEvent = await matrixClient.getStateEvent(iframeRoomId, 'm.room.name').catch(() => {});
             setTitle(nameEvent?.name);
         };
 
         if (!cancelled) fetchRoomName();
 
-        return () => cancelled = true;
+        return () => (cancelled = true);
     }, [iframeRoomId, matrixClient, parsedTitle]);
 
     const CurrentView = () => {
         switch (currentTemplate) {
             case 'studentproject':
-                return <ProjectView content={iframeRoomId} />;
+                return <ProjectView content={iframeUrl} />;
             case 'etherpad':
                 return (
                     <>
@@ -41,8 +47,9 @@ const ExploreIframeViews = ({ currentTemplate, iframeRoomId, title: parsedTitle 
                             content={matrix.roomContents.get(iframeRoomId)?.body}
                             title={title}
                             removeLink={() => console.log('removing pad from parent')}
-                            removingLink={false} />
-                        <iframe title="studentproject" src={matrix.roomContents.get(iframeRoomId)?.body} />
+                            removingLink={false}
+                        />
+                        <iframe title="etherpad" src={iframeUrl} />
                     </>
                 );
             case 'spacedeck':
@@ -52,17 +59,13 @@ const ExploreIframeViews = ({ currentTemplate, iframeRoomId, title: parsedTitle 
                             content={matrix.roomContents.get(iframeRoomId)?.body}
                             title={title}
                             removeLink={() => console.log('removing sketch from parent')}
-                            removingLink={false} />
-                        <iframe title="sketch" src={matrix.roomContents.get(iframeRoomId)?.body} />
+                            removingLink={false}
+                        />
+                        <iframe title="sketch" src={iframeUrl} />
                     </>
                 );
             default:
-                return (
-
-                    <ChatIframeView title="chat"
-                        src={`${getConfig().publicRuntimeConfig.chat.pathToElement}/#/room/${iframeRoomId}`} />
-
-                );
+                return <ChatIframeView title="chat" src={`${getConfig().publicRuntimeConfig.chat.pathToElement}/#/room/${iframeRoomId}`} />;
         }
     };
 
