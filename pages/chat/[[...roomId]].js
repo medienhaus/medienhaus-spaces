7b07384db4a39ac3817b19715497ab2a0fffb6bd
Florian Weber <florian@weber.world>
2024-03-06 18:27:47 +0100

    Try another way of clicking buttons inside the <iframe>

--- pages/chat/[[...roomId]].js
+++ pages/chat/[[...roomId]].js
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import getConfig from 'next/config';
@@ -136,6 +136,14 @@ export default function Chat() {
         router.push('/chat');
     };
 
+    const toggleCall = () => {
+        iframe.current.contentDocument.querySelector('header.mx_RoomHeader > div button:nth-child(1)').click();
+    };
+
+    const toggleThreads = () => {
+        iframe.current.contentDocument.querySelector('header.mx_RoomHeader > div button:nth-child(2)').click();
+    };
+
     const directMessages = _.sortBy([...matrix.directMessages.values()], sortRooms);
     // Other rooms contains all rooms, except for the ones that ...
     const otherRooms = _([...matrix.rooms.values()])
@@ -235,26 +243,12 @@ export default function Chat() {
                                             <RiDoorOpenLine />
                                         </Icon>
                                     </TextButton>
-                                    <TextButton
-                                        title={t('Call')}
-                                        onClick={() =>
-                                            iframe.current.contentDocument
-                                                .querySelector('header.mx_RoomHeader > div button:nth-child(1)')
-                                                .click()
-                                        }
-                                    >
+                                    <TextButton title={t('Call')} onClick={toggleCall}>
                                         <Icon>
                                             <RiPhoneLine />
                                         </Icon>
                                     </TextButton>
-                                    <TextButton
-                                        title={t('Threads')}
-                                        onClick={() =>
-                                            iframe.current.contentDocument
-                                                .querySelector('header.mx_RoomHeader > div button:nth-child(2)')
-                                                .click()
-                                        }
-                                    >
+                                    <TextButton title={t('Threads')} onClick={toggleThreads}>
                                         <Icon>
                                             <RiSidebarFoldLine />
                                         </Icon>
