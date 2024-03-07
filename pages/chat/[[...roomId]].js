1dc910009185ca6e74943c4030e8aa03238c490f
Florian Weber <florian@weber.world>
2024-03-07 13:15:31 +0100

    Remove unused import

--- pages/chat/[[...roomId]].js
+++ pages/chat/[[...roomId]].js
@@ -1,4 +1,4 @@
-import React, { useCallback, useEffect, useRef } from 'react';
+import React, { useEffect, useRef } from 'react';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 import getConfig from 'next/config';
