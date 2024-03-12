ed02865852a07dec8d11c9741673aa3db3b1bae2
Marcel <79279756+aofn@users.noreply.github.com>
2024-03-12 16:32:24 +0100

    refactor: return null if onboarding is not defined

--- components/onboarding/onboardingPilot.js
+++ components/onboarding/onboardingPilot.js
@@ -13,9 +13,11 @@ const OnboardingPilot = () => {
     const [side, setSide] = useState('onboardingBottomRight');
     const [isOpen, setIsOpen] = useState(true);
 
+    if (!onboarding) return null;
+
     return (
         <>
-            {onboarding?.active && (
+            {onboarding.active && (
                 <Sheet open={isOpen} onOpenChange={setIsOpen} modal={false}>
                     <SheetContent
                         side={side}
@@ -34,23 +36,23 @@ const OnboardingPilot = () => {
                                 {side === 'onboardingBottomRight' ? '\u035F' : '⌅'}
                             </Button>
                             <SheetTitle>
-                                {onboarding?.currentRoute} — {onboarding?.currentStepTitle}
+                                {onboarding.currentRoute} — {onboarding.currentStepTitle}
                             </SheetTitle>
-                            <SheetDescription>{onboarding?.currentStepDescription}</SheetDescription>
+                            <SheetDescription>{onboarding.currentStepDescription}</SheetDescription>
                         </SheetHeader>
 
                         <SheetFooter className="mt-8 grid grid-cols-2 gap-4">
                             <Button
-                                disabled={!onboarding?.hasPrev}
+                                disabled={!onboarding.hasPrev}
                                 onClick={() => {
                                     onboarding.processStep(-1);
                                 }}
                             >
                                 {t('prev')}
                             </Button>
-                            {onboarding?.active && onboarding?.hasNext && (
+                            {onboarding.active && onboarding.hasNext && (
                                 <Button
-                                    disabled={!onboarding?.hasNext}
+                                    disabled={!onboarding.hasNext}
                                     onClick={() => {
                                         onboarding.processStep(1);
                                     }}
@@ -59,19 +61,19 @@ const OnboardingPilot = () => {
                                 </Button>
                             )}
 
-                            {onboarding?.active && !onboarding?.hasNext && onboarding?.nextRouteName.length > 0 && (
+                            {onboarding.active && !onboarding.hasNext && onboarding.nextRouteName.length > 0 && (
                                 <Button
                                     onClick={() => {
-                                        onboarding?.nextRoute();
+                                        onboarding.nextRoute();
                                     }}
                                 >
-                                    {'\uE1D8'} {onboarding?.nextRouteName}
+                                    {'\uE1D8'} {onboarding.nextRouteName}
                                 </Button>
                             )}
-                            {onboarding?.active && !onboarding?.hasNext && !onboarding?.nextRouteName.length > 0 && (
+                            {onboarding.active && !onboarding.hasNext && !onboarding.nextRouteName.length > 0 && (
                                 <Button
                                     onClick={() => {
-                                        onboarding?.exit();
+                                        onboarding.exit();
                                     }}
                                 >
                                     {t('close')}
