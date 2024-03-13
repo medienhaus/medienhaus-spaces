bcccfd4dc6b0bf886caf33f306cd3634bcf5977c
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-03-13 16:13:50 +0100

    draft: change tabs position (and code location)

--- components/UI/ServiceIframeHeader.js
+++ components/UI/ServiceIframeHeader.js
@@ -1,7 +1,6 @@
 import { useTranslation } from 'react-i18next';
 import React from 'react';
-// import React, { useState } from 'react';
-import { RiDeleteBinLine, RiFolderLine, RiGroupLine, RiListSettingsLine } from '@remixicon/react';
+import { RiDeleteBinLine } from '@remixicon/react';
 
 import CopyToClipboard from '../../components/UI/CopyToClipboard';
 import LoadingSpinnerInline from '../../components/UI/LoadingSpinnerInline';
@@ -10,40 +9,9 @@ import DefaultLayout from '../layouts/default';
 import KnockOnMatrixRoom from './KnockOnMatrixRoom';
 import AddFavourite from './favourites/AddFavourite';
 import Icon from '@/components/UI/Icon';
-// import ExploreMatrixActions from '../../pages/explore/manage-room/ExploreMatrixActions';
-import { Separator } from '@/components/UI/shadcn/Separator';
-import { Tabs, TabsList, TabsTrigger } from '@/components/UI/shadcn/Tabs';
-// import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/UI/shadcn/Tabs';
 
-/*
-const ToggleButton = styled.button`
-    width: unset;
-    height: calc(var(--margin) * 1.3);
-    padding: unset;
-    background-color: unset;
-    border: unset;
-`;
-*/
-
-// @TODO check if user actually has the needed power level to invite users to the matrix room
-const ServiceIframeHeader = ({
-    isDeletingPad,
-    deleteContent,
-    title,
-    roomId,
-    myPadsObject,
-    content,
-    myPowerLevel,
-    activeContentView,
-    setActiveContentView,
-    joinRule,
-    // isInviteUsersOpen,
-    // setIsInviteUsersOpen,
-    // setSettingsTabValue,
-}) => {
+const ServiceIframeHeader = ({ isDeletingPad, deleteContent, title, roomId, myPadsObject, content, joinRule }) => {
     const { t } = useTranslation(['explore', 'etherpad']);
-
-    // const [settingsTabValue, setSettingsTabValue] = useState('settings');
 
     return (
         <DefaultLayout.IframeHeader>
@@ -69,66 +37,6 @@ const ServiceIframeHeader = ({
                 )}
 
                 <AddFavourite roomId={roomId} />
-
-                {myPowerLevel && (
-                    <>
-                        <Separator orientation="vertical" />
-
-                        <Tabs className="[&>[role=tabpanel]]:pt-6" onValueChange={setActiveContentView} value={activeContentView}>
-                            <TabsList>
-                                <TabsTrigger
-                                    onClick={() => {
-                                        setActiveContentView('content');
-                                    }}
-                                    title={t('Show contexts and items of {{name}}', { name: title })}
-                                    value="content"
-                                >
-                                    <Icon>
-                                        <RiFolderLine />
-                                    </Icon>
-                                </TabsTrigger>
-
-                                <TabsTrigger
-                                    onClick={() => {
-                                        setActiveContentView('members');
-                                    }}
-                                    title={t('Show members of {{name}}', { name: title })}
-                                    value="members"
-                                >
-                                    <Icon>
-                                        <RiGroupLine />
-                                    </Icon>
-                                </TabsTrigger>
-
-                                <TabsTrigger
-                                    onClick={() => {
-                                        setActiveContentView('settings');
-                                    }}
-                                    title={t('Show settings of {{name}}', { name: title })}
-                                    value="advanced"
-                                >
-                                    <Icon>
-                                        <RiListSettingsLine />
-                                    </Icon>
-                                </TabsTrigger>
-                            </TabsList>
-
-                            {/*
-                            <TabsContent className="pb-6 [&>*+*]:mt-8" value="content">
-                                foo
-                            </TabsContent>
-
-                            <TabsContent className="pb-6 [&>*+*]:mt-8" value="members">
-                                bar
-                            </TabsContent>
-
-                            <TabsContent className="pb-6 [&>*+*]:mt-8" value="settings">
-                                bar
-                            </TabsContent>
-                            */}
-                        </Tabs>
-                    </>
-                )}
             </DefaultLayout.IframeHeaderButtonWrapper>
         </DefaultLayout.IframeHeader>
     );
