ea1e463d38b37ca6a5a323018ef97b348568e125
Marcel <79279756+aofn@users.noreply.github.com>
2024-03-13 13:15:53 +0100

    refactor: implement progress bar for explore instead of displaying loading spinner

---
+++ components/UI/shadcn/Progress.js
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import * as ProgressPrimitive from '@radix-ui/react-progress';
+
+import { cn } from '@/lib/utils';
+
+const Progress = React.forwardRef(({ className, value, ...props }, ref) => (
+    <ProgressPrimitive.Root ref={ref} className={cn('relative h-1 w-full overflow-hidden  bg-secondary', className)} {...props}>
+        <ProgressPrimitive.Indicator
+            className="h-full w-full flex-1 bg-accent transition-all"
+            style={{ transform: `translateX(-${100 - (value || 0)}%)` }}
+        />
+    </ProgressPrimitive.Root>
+));
+Progress.displayName = ProgressPrimitive.Root.displayName;
+
+export { Progress };
