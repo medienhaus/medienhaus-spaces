9b59502b2ef94c1086c1f519b85380435d153a9d
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-03-13 15:48:50 +0100

    style: fix TabList inline flex positioning

--- components/UI/shadcn/Tabs.js
+++ components/UI/shadcn/Tabs.js
@@ -18,7 +18,7 @@ const TabsTrigger = React.forwardRef(({ className, ...props }, ref) => (
     <TabsPrimitive.Trigger
         ref={ref}
         className={cn(
-            'flex-1 justify-center whitespace-nowrap rounded-sm px-3 py-1.5 ring-offset-background transition-all hover:text-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
+            'inline-flex flex-1 items-center justify-center whitespace-nowrap rounded-sm px-3 py-1.5 ring-offset-background transition-all hover:text-accent focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50 data-[state=active]:bg-background data-[state=active]:text-foreground data-[state=active]:shadow-sm',
             className,
         )}
         {...props}
