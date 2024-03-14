0aa8947eca50a86d29345feb1f1a77dd7411e8d4
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-03-14 16:29:33 +0100

    style: clean-up and refine Sheet component

--- components/UI/shadcn/Sheet.js
+++ components/UI/shadcn/Sheet.js
@@ -16,7 +16,7 @@ const SheetPortal = SheetPrimitive.Portal;
 const SheetOverlay = React.forwardRef(({ className, ...props }, ref) => (
     <SheetPrimitive.Overlay
         className={cn(
-            'fixed inset-0 z-50 bg-black/80  data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
+            'fixed inset-0 z-50 bg-black/80 data-[state=open]:animate-in data-[state=closed]:animate-out data-[state=closed]:fade-out-0 data-[state=open]:fade-in-0',
             className,
         )}
         {...props}
@@ -33,24 +33,25 @@ const sheetVariants = cva(
                 top: 'inset-x-0 top-0 border-b data-[state=closed]:slide-out-to-top data-[state=open]:slide-in-from-top',
                 bottom: 'inset-x-0 bottom-0 border-t data-[state=closed]:slide-out-to-bottom data-[state=open]:slide-in-from-bottom',
                 left: 'inset-y-0 left-0 h-full w-3/4 border-r data-[state=closed]:slide-out-to-left data-[state=open]:slide-in-from-left sm:max-w-sm',
-                right: 'inset-y-0 right-0 h-full w-3/4  border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
-                onboardingBottomRight: 'fixed md:right-8 md:bottom-8 bottom-0  md:w-1/4 rounded-lg bg-onboarding w-full md:p-8 p-3',
-                onboardingMinimized:
-                    'fixed md:right-8 md:bottom-8 md:w-20 md:h-16 rounded-lg bg-onboarding overflow-hidden md:p-3 w-full right-0 bottom-0 h-8 p-0',
+                right: 'inset-y-0 right-0 h-full w-3/4 border-l data-[state=closed]:slide-out-to-right data-[state=open]:slide-in-from-right sm:max-w-sm',
+                float:
+                    '[&>button]:absolute [&>button]:top-3 [&>button]:right-3 bg-accent-foreground text-white fixed bottom-0 w-full rounded-lg p-4 md:bottom-6 md:right-6 md:w-1/4 md:p-6',
+                minified:
+                    'fixed bottom-4 right-4 grid w-fit items-center overflow-hidden rounded-lg bg-accent-foreground p-2 text-white md:bottom-6 md:right-6 [&>:not(button)]:hidden',
             },
         },
         defaultVariants: {
-            side: 'onboardingBottomRight',
+            side: 'float',
         },
     },
 );
 
-const SheetContent = React.forwardRef(({ side = 'onboardingBottomRight', className, children, ...props }, ref) => (
+const SheetContent = React.forwardRef(({ side = 'float', className, children, ...props }, ref) => (
     <SheetPortal>
         <SheetOverlay />
         <SheetPrimitive.Content ref={ref} className={cn(sheetVariants({ side }), className)} {...props}>
             {children}
-            {!(side === 'onboardingMinimized' || side === 'onboardingBottomRight') && (
+            {!(side === 'minified' || side === 'float') && (
                 <SheetPrimitive.Close className="absolute right-4 top-4 rounded-sm opacity-70 ring-offset-background transition-opacity hover:opacity-100 focus:outline-none focus:ring-2 focus:ring-ring focus:ring-offset-2 disabled:pointer-events-none data-[state=open]:bg-secondary">
                     <X className="h-4 w-4" />
                     <span className="sr-only">Close</span>
@@ -61,23 +62,19 @@ const SheetContent = React.forwardRef(({ side = 'onboardingBottomRight', classNa
 ));
 SheetContent.displayName = SheetPrimitive.Content.displayName;
 
-const SheetHeader = ({ className, ...props }) => (
-    <div className={cn('flex flex-col space-y-2 text-center sm:text-left', className)} {...props} />
-);
+const SheetHeader = ({ className, ...props }) => <div className={cn('flex flex-col space-y-4', className)} {...props} />;
 SheetHeader.displayName = 'SheetHeader';
 
-const SheetFooter = ({ className, ...props }) => (
-    <div className={cn('flex flex-col-reverse sm:flex-row sm:justify-end sm:space-x-2', className)} {...props} />
-);
+const SheetFooter = ({ className, ...props }) => <div className={cn('flex flex-col-reverse', className)} {...props} />;
 SheetFooter.displayName = 'SheetFooter';
 
 const SheetTitle = React.forwardRef(({ className, ...props }, ref) => (
-    <SheetPrimitive.Title ref={ref} className={cn('text-lg font-semibold text-foreground', className)} {...props} />
+    <SheetPrimitive.Title ref={ref} className={cn('', className)} {...props} />
 ));
 SheetTitle.displayName = SheetPrimitive.Title.displayName;
 
 const SheetDescription = React.forwardRef(({ className, ...props }, ref) => (
-    <SheetPrimitive.Description ref={ref} className={cn('text-sm text-muted-foreground', className)} {...props} />
+    <SheetPrimitive.Description ref={ref} className={cn('', className)} {...props} />
 ));
 SheetDescription.displayName = SheetPrimitive.Description.displayName;
 
