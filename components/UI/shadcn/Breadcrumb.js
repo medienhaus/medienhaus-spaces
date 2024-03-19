132df440f6d01fc4447543e85c613156cb39f18d
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-03-19 16:55:17 +0100

    style: no muted foreground color in breadcrumb list

--- components/UI/shadcn/Breadcrumb.js
+++ components/UI/shadcn/Breadcrumb.js
@@ -8,11 +8,7 @@ const Breadcrumb = React.forwardRef(({ ...props }, ref) => <nav ref={ref} aria-l
 Breadcrumb.displayName = 'Breadcrumb';
 
 const BreadcrumbList = React.forwardRef(({ className, ...props }, ref) => (
-    <ol
-        ref={ref}
-        className={cn('flex flex-wrap items-center gap-1.5 break-words text-muted-foreground sm:gap-2.5', className)}
-        {...props}
-    />
+    <ol ref={ref} className={cn('flex flex-wrap items-center gap-1.5 break-words sm:gap-2.5', className)} {...props} />
 ));
 BreadcrumbList.displayName = 'BreadcrumbList';
 
