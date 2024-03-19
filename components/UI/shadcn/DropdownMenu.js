d2f407d66d14af3ea73c445e9e3bb0c814ee2cea
Andi Rueckel <andirueckel@users.noreply.github.com>
2024-03-19 14:24:08 +0100

    style: fix focus/hover color of a:link in dropdown

--- components/UI/shadcn/DropdownMenu.js
+++ components/UI/shadcn/DropdownMenu.js
@@ -63,7 +63,7 @@ const DropdownMenuItem = React.forwardRef(({ className, inset, ...props }, ref) 
     <DropdownMenuPrimitive.Item
         ref={ref}
         className={cn(
-            'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 outline-none transition-colors focus:bg-accent focus:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50',
+            'relative flex cursor-default select-none items-center rounded-sm px-2 py-1.5 outline-none transition-colors focus:bg-accent focus:text-white data-[disabled]:pointer-events-none data-[disabled]:opacity-50 [&_a:link]:focus:text-white',
             inset && 'pl-8',
             className,
         )}
