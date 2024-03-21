527c4f12e0f4aadafba51f2a503f830812380f1e
Marcel <79279756+aofn@users.noreply.github.com>
2024-03-21 14:42:11 +0100

    refactor: simplify onboardingContext.js by using useImmer and returning one object for the current onboarding route. use one `changeRoute` method instead of two separate next and previous route methods. Als let `processStep()` decide whether to change the route or go to the next step.
    add returns to useEffects

--- components/onboarding/onboardingContext.js
+++ components/onboarding/onboardingContext.js
@@ -1,8 +1,10 @@
 import { createContext, useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { driver } from 'driver.js'; // import driver.js
-
 import 'driver.js/dist/driver.css'; //import css
+import { useImmer } from 'use-immer';
+import _ from 'lodash';
+
 import onboardingScriptGeneric from './onboardingScriptGeneric.json';
 import onboardingScriptCustom from './onboardingScriptCustom.json';
 
@@ -11,23 +13,27 @@ const OnboardingContext = createContext(undefined);
 function useOnboardingProvider() {
     const router = useRouter();
     const [active, setActive] = useState(false);
+    const [route, setRoute] = useImmer({
+        name: '',
+        index: 0,
+        steps: [],
+        hasNext: false,
+        hasPrev: false,
+        description: '',
+        title: '',
+    });
 
-    const [currentRoute, setCurrentRoute] = useState('');
-    const [nextRouteName, setNextRouteName] = useState('');
-
-    const [prevRouteName, setPrevRouteName] = useState('');
-
-    const [currentRouteIndex, setCurrentRouteIndex] = useState(0);
-
-    const [currentSteps, setCurrentSteps] = useState([]);
-
-    const [currentStep, setCurrentStep] = useState(0);
-    const [hasPrev, setHasPrev] = useState(false);
-    const [hasNext, setHasNext] = useState(false);
-    const [currentStepDescription, setCurrentStepDescription] = useState('');
-    const [currentStepTitle, setCurrentStepTitle] = useState('');
-
-    const [driverJsConfig, setDriverJsConfig] = useState(null);
+    const [driverJsConfig, setDriverJsConfig] = useImmer({
+        allowClose: false,
+        showProgress: false,
+        overlayColor: 'black',
+        overlayOpacity: 0.0,
+        animate: false,
+        popoverClass: 'driverJsPopOver',
+        disableButtons: ['next', 'previous', 'close'],
+        showButtons: [],
+        steps: [],
+    });
 
     const [tourInstance, setTourInstance] = useState(null);
 
@@ -36,163 +42,143 @@ function useOnboardingProvider() {
     const isScriptCustom = onboardingScriptCustom?.length > 0;
 
     const startTour = (position = 0) => {
+        if (!onboardingScript[position]) return;
+
         if (position > onboardingScript.length - 1) {
             position = 0;
         }
 
         // start the tour with the first route and its steps. reset everything to the initial state
-        setCurrentRoute(onboardingScript[position].route);
-        setCurrentSteps(onboardingScript[position].steps);
-        setCurrentRouteIndex(position);
-        setCurrentStep(0);
+        setRoute((draft) => {
+            draft.name = onboardingScript[position].route;
+            draft.index = position;
+            draft.steps = onboardingScript[position].steps;
+            draft.prevRoute = position > 0 && onboardingScript[position - 1].route;
+            draft.nextRoute = onboardingScript[draft.index + 1]?.route;
+            draft.isLastStep = onboardingScript[position].length > 1;
+            draft.isFirstStep = true;
+        });
         setActive(true);
-
-        if (position > 0) {
-            setPrevRouteName(onboardingScript[position - 1].route);
-        }
-
         if (onboardingScript[position].route) router.push(onboardingScript[position].route);
     };
 
     useEffect(() => {
+        let cancelled = false;
+        if (cancelled) return;
         // set the config for driver.js with the current steps of the active route
-        setDriverJsConfig({
-            allowClose: false,
-            showProgress: false,
-            overlayColor: 'black',
-            overlayOpacity: 0.0,
-            animate: false,
-            popoverClass: 'driverJsPopOver',
-            disableButtons: ['next', 'previous', 'close'],
-            showButtons: [],
-            steps: currentSteps,
+        if (_.isEmpty(route.steps)) return;
+
+        setDriverJsConfig((draft) => {
+            if (draft.steps === route.steps) return;
+            draft.steps = route.steps;
         });
-    }, [currentSteps]);
+
+        return () => (cancelled = true);
+        // we need to disable the eslint exhaustive-deps rule because of useImmer
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [route.steps]);
 
     useEffect(() => {
+        let cancelled = false;
+        if (cancelled) return;
         // create tour instance with the driver.js config if populated
-        if (currentSteps.length > 0) {
-            setTourInstance(driver(driverJsConfig));
-        }
-    }, [driverJsConfig, currentSteps]);
+        if (!driverJsConfig.steps.length) return;
+
+        setTourInstance(driver(driverJsConfig));
+
+        return () => (cancelled = true);
+    }, [driverJsConfig]);
 
     useEffect(() => {
+        let cancelled = false;
+        if (cancelled) return;
+
         // start the tour if the instance is created, which is the case after a route change
         if (tourInstance) {
             tourInstance.drive();
-            setCurrentStepDescription(tourInstance.getActiveStep()?.popover?.description);
-            setCurrentStepTitle(tourInstance.getActiveStep()?.popover?.title);
+            setRoute((draft) => {
+                draft.title = tourInstance.getActiveStep().popover.title;
+                draft.description = tourInstance.getActiveStep().popover.description;
+            });
             tourInstance.refresh();
-            setHasPrev(!tourInstance.isFirstStep());
-            setHasNext(!tourInstance.isLastStep());
         }
+
+        return () => (cancelled = true);
+        // we need to disable the eslint exhaustive-deps rule because of useImmer
+        // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [tourInstance]);
 
-    useEffect(() => {
-        // set the next route name if there are more routes in the onboarding script. the nextRoutName is used in the onboarding Pilot component to display the next route name if the last step of the current route is reached
-        if (!hasNext && currentRouteIndex + 1 < onboardingScript.length) {
-            setNextRouteName(onboardingScript[currentRouteIndex + 1].route);
-        } else {
-            setNextRouteName('');
-        }
-    }, [currentRouteIndex, hasNext, onboardingScript]);
+    const changeRoute = (direction) => {
+        setRoute((draft) => {
+            const newIndex = draft.index + direction;
 
-    const nextRoute = () => {
-        // if there are more routes in the onboarding script, move to the next route
-        if (currentRouteIndex + 1 < onboardingScript.length) {
-            setPrevRouteName(currentRoute);
-            const newIndex = currentRouteIndex + 1;
-            setCurrentRouteIndex(newIndex);
-            setCurrentRoute(onboardingScript[newIndex].route);
-            setCurrentSteps(onboardingScript[newIndex].steps);
-            if (onboardingScript[newIndex].route) router.push(onboardingScript[newIndex].route);
-        }
-    };
+            if (newIndex >= 0 && newIndex < onboardingScript.length) {
+                draft.index = newIndex;
+                draft.name = onboardingScript[newIndex].route;
+                draft.steps = onboardingScript[newIndex].steps;
+                draft.isLastStep = onboardingScript[newIndex].length > 1;
+                // when changing routes, we always jump to the first step of that route.
+                draft.isFirstStep = true;
+                draft.nextRoute = onboardingScript[newIndex + 1]?.route;
+                draft.prevRoute = onboardingScript[newIndex - 1]?.route;
 
-    const prevRoute = () => {
-        if (currentRouteIndex - 1 >= 0) {
-            const newIndex = currentRouteIndex - 1;
-            setCurrentRouteIndex(newIndex);
-            setCurrentRoute(onboardingScript[newIndex].route);
-            setCurrentSteps(onboardingScript[newIndex].steps);
-            if (onboardingScript[newIndex].route) router.push(onboardingScript[newIndex].route);
-
-            if (newIndex > 0) {
-                setPrevRouteName(onboardingScript[newIndex - 1].route);
-            } else {
-                setPrevRouteName('');
+                if (onboardingScript[newIndex].route) router.push(onboardingScript[newIndex].route);
             }
-        }
+        });
     };
 
     const processStep = (offset) => {
         if (!tourInstance.isActive()) {
             // just to make sure, if the tour instance is not active, start the tour
             tourInstance.drive();
-            setCurrentStepDescription(tourInstance.getActiveStep().popover.description);
-            setCurrentStepTitle(tourInstance.getActiveStep().popover.title);
+            setRoute((draft) => {
+                draft.title = tourInstance.getActiveStep().popover.title;
+                draft.description = tourInstance.getActiveStep().popover.description;
+            });
 
             return;
         }
 
         if (offset > 0 && !tourInstance.isLastStep()) {
             tourInstance.moveNext();
-            setCurrentStep(tourInstance.getActiveIndex() + 1);
         } else if (offset < 0 && !tourInstance.isFirstStep()) {
             tourInstance.movePrevious();
-            setCurrentStep(tourInstance.getActiveIndex() - 1);
         }
 
-        setCurrentStepDescription(tourInstance.getActiveStep().popover.description);
-        setCurrentStepTitle(tourInstance.getActiveStep().popover.title);
-
-        tourInstance.refresh();
-        setHasPrev(!tourInstance.isFirstStep());
-        setHasNext(!tourInstance.isLastStep());
+        if ((offset > 0 && route.isLastStep) || (offset < 0 && route.isFirstStep)) {
+            changeRoute(offset);
+        } else {
+            setRoute((draft) => {
+                draft.title = tourInstance.getActiveStep().popover.title;
+                draft.description = tourInstance.getActiveStep().popover.description;
+                draft.isLastStep = tourInstance.isLastStep();
+                draft.isFirstStep = tourInstance.isFirstStep();
+            });
+            tourInstance.refresh();
+        }
     };
 
     const exit = () => {
         setActive(false);
         tourInstance?.destroy();
-        setCurrentRouteIndex(0);
-        setCurrentSteps([]);
-    };
-
-    const writeOnboardStateToAccountData = async (matrixClient, overwrittenData) => {
-        const data = {
-            active: active,
-            currentRouteIndex: currentRouteIndex,
-        };
-
-        Object.keys(overwrittenData).forEach((key) => {
-            data[key] = overwrittenData[key];
+        setRoute((draft) => {
+            draft.name = '';
+            draft.index = 0;
+            draft.steps = [];
+            draft.description = '';
+            draft.title = '';
         });
-
-        await matrixClient.setAccountData('dev.medienhaus.spaces.onboarding', data);
     };
 
     return {
         active,
         setActive,
-        setCurrentRoute,
-        currentRoute,
-        currentSteps,
-        currentRouteIndex,
-        currentStep,
-        hasPrev,
-        hasNext,
-        currentStepDescription,
-        currentStepTitle,
         tourInstance,
         startTour,
         processStep,
-        prevRoute,
-        nextRoute,
-        prevRouteName,
-        nextRouteName,
+        route,
         exit,
         isScriptCustom,
-        writeOnboardStateToAccountData,
     };
 }
 
