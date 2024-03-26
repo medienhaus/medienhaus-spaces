03240c477556b0d5dbece374981af1b215435978
Marcel <79279756+aofn@users.noreply.github.com>
2024-03-26 12:10:43 +0100

    refactor: create helper functions to make file easier to read, use `useCallback` for callback funtions

--- components/onboarding/onboardingContext.js
+++ components/onboarding/onboardingContext.js
@@ -1,9 +1,8 @@
-import { createContext, useContext, useEffect, useState } from 'react';
+import { createContext, useCallback, useContext, useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
 import { driver } from 'driver.js'; // import driver.js
 import 'driver.js/dist/driver.css'; //import css
 import { useImmer } from 'use-immer';
-import _ from 'lodash';
 
 import onboardingScriptGeneric from './onboardingScriptGeneric.json';
 import onboardingScriptCustom from './onboardingScriptCustom.json';
@@ -41,32 +40,24 @@ function useOnboardingProvider() {
 
     const isScriptCustom = onboardingScriptCustom?.length > 0;
 
-    const startTour = (position = 0) => {
-        if (!onboardingScript[position]) return;
+    const startTour = useCallback(
+        (position = 0) => {
+            if (!onboardingScript[position]) return;
 
-        if (position > onboardingScript.length - 1) {
-            position = 0;
-        }
+            if (position > onboardingScript.length - 1) {
+                position = 0;
+            }
 
-        // start the tour with the first route and its steps. reset everything to the initial state
-        setRoute((draft) => {
-            draft.name = onboardingScript[position].route;
-            draft.index = position;
-            draft.steps = onboardingScript[position].steps;
-            draft.prevRoute = position > 0 && onboardingScript[position - 1].route;
-            draft.nextRoute = onboardingScript[draft.index + 1]?.route;
-            draft.isLastStep = onboardingScript[position].length > 1;
-            draft.isFirstStep = true;
-        });
-        setActive(true);
-        if (onboardingScript[position].route) router.push(onboardingScript[position].route);
-    };
+            setRoute((draft) => updateRouteState(draft, onboardingScript, position));
+            setActive(true);
+            if (onboardingScript[position].route) router.push(onboardingScript[position].route);
+        },
+        [onboardingScript, router, setRoute],
+    );
 
     useEffect(() => {
         let cancelled = false;
         if (cancelled) return;
-        // set the config for driver.js with the current steps of the active route
-        if (_.isEmpty(route.steps)) return;
 
         setDriverJsConfig((draft) => {
             if (draft.steps === route.steps) return;
@@ -74,9 +65,7 @@ function useOnboardingProvider() {
         });
 
         return () => (cancelled = true);
-        // we need to disable the eslint exhaustive-deps rule because of useImmer
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [route.steps]);
+    }, [route.steps, setDriverJsConfig]);
 
     useEffect(() => {
         let cancelled = false;
@@ -93,75 +82,57 @@ function useOnboardingProvider() {
         let cancelled = false;
         if (cancelled) return;
 
-        // start the tour if the instance is created, which is the case after a route change
         if (tourInstance) {
             tourInstance.drive();
-            setRoute((draft) => {
-                draft.title = tourInstance.getActiveStep().popover.title;
-                draft.description = tourInstance.getActiveStep().popover.description;
-            });
+            setRoute((draft) => updateRouteTitle(draft, tourInstance));
             tourInstance.refresh();
         }
 
         return () => (cancelled = true);
-        // we need to disable the eslint exhaustive-deps rule because of useImmer
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [tourInstance]);
+    }, [tourInstance, setRoute]);
 
-    const changeRoute = (direction) => {
-        setRoute((draft) => {
-            const newIndex = draft.index + direction;
+    const changeRoute = useCallback(
+        (direction) => {
+            setRoute((draft) => updateRouteOnChange(draft, onboardingScript, direction, router));
+        },
+        [onboardingScript, router, setRoute],
+    );
 
-            if (newIndex >= 0 && newIndex < onboardingScript.length) {
-                draft.index = newIndex;
-                draft.name = onboardingScript[newIndex].route;
-                draft.steps = onboardingScript[newIndex].steps;
-                draft.isLastStep = onboardingScript[newIndex].length > 1;
-                // when changing routes, we always jump to the first step of that route.
-                draft.isFirstStep = true;
-                draft.nextRoute = onboardingScript[newIndex + 1]?.route;
-                draft.prevRoute = onboardingScript[newIndex - 1]?.route;
+    const processStep = useCallback(
+        (offset) => {
+            if (!tourInstance.isActive()) {
+                tourInstance.drive();
+                setRoute((draft) => updateRouteTitle(draft, tourInstance));
 
-                if (onboardingScript[newIndex].route) router.push(onboardingScript[newIndex].route);
+                return;
             }
-        });
-    };
 
-    const processStep = (offset) => {
-        if (!tourInstance.isActive()) {
-            // just to make sure, if the tour instance is not active, start the tour
-            tourInstance.drive();
-            setRoute((draft) => {
-                draft.title = tourInstance.getActiveStep().popover.title;
-                draft.description = tourInstance.getActiveStep().popover.description;
-            });
+            if ((offset > 0 && !tourInstance.isLastStep()) || (offset < 0 && !tourInstance.isFirstStep())) {
+                offset > 0 ? tourInstance.moveNext() : tourInstance.movePrevious();
+            }
 
-            return;
-        }
+            if (offset > 0 && route.isLastStep && !route.nextRoute) {
+                // Handle the last step
+                setRoute((draft) => updateRouteStepState(draft, tourInstance));
+                tourInstance.refresh();
 
-        if (offset > 0 && !tourInstance.isLastStep()) {
-            tourInstance.moveNext();
-        } else if (offset < 0 && !tourInstance.isFirstStep()) {
-            tourInstance.movePrevious();
-        }
+                return;
+            }
 
-        if ((offset > 0 && route.isLastStep) || (offset < 0 && route.isFirstStep)) {
-            changeRoute(offset);
-        } else {
-            setRoute((draft) => {
-                draft.title = tourInstance.getActiveStep().popover.title;
-                draft.description = tourInstance.getActiveStep().popover.description;
-                draft.isLastStep = tourInstance.isLastStep();
-                draft.isFirstStep = tourInstance.isFirstStep();
-            });
-            tourInstance.refresh();
-        }
-    };
+            if ((offset > 0 && route.isLastStep) || (offset < 0 && route.isFirstStep)) {
+                changeRoute(offset);
+            } else {
+                setRoute((draft) => updateRouteStepState(draft, tourInstance));
+                tourInstance.refresh();
+            }
+        },
+        [changeRoute, route.isFirstStep, route.isLastStep, route.nextRoute, setRoute, tourInstance],
+    );
 
-    const exit = () => {
+    const exit = useCallback(() => {
         setActive(false);
         tourInstance?.destroy();
-    };
+    }, [tourInstance]);
 
     return {
         active,
@@ -179,3 +150,42 @@ function useOnboarding() {
 }
 
 export { OnboardingContext, useOnboardingProvider, useOnboarding };
+
+// Helper functions
+const updateRouteState = (draft, onboardingScript, position) => {
+    draft.index = position;
+    draft.name = onboardingScript[position].route;
+    draft.steps = onboardingScript[position].steps;
+    draft.prevRoute = position > 0 && onboardingScript[position - 1].route;
+    draft.nextRoute = onboardingScript[draft.index + 1]?.route;
+    draft.isLastStep = onboardingScript[position].length > 1;
+    draft.isFirstStep = true;
+};
+
+const updateRouteTitle = (draft, tourInstance) => {
+    draft.title = tourInstance.getActiveStep().popover.title;
+    draft.description = tourInstance.getActiveStep().popover.description;
+};
+
+const updateRouteOnChange = (draft, onboardingScript, direction, router) => {
+    const newIndex = draft.index + direction;
+
+    if (newIndex >= 0 && newIndex < onboardingScript.length) {
+        draft.index = newIndex;
+        draft.name = onboardingScript[newIndex].route;
+        draft.steps = onboardingScript[newIndex].steps;
+        draft.isLastStep = onboardingScript[newIndex].length > 1;
+        draft.isFirstStep = true;
+        draft.nextRoute = onboardingScript[newIndex + 1]?.route;
+        draft.prevRoute = onboardingScript[newIndex - 1]?.route;
+
+        if (onboardingScript[newIndex].route) router.push(onboardingScript[newIndex].route);
+    }
+};
+
+const updateRouteStepState = (draft, tourInstance) => {
+    draft.title = tourInstance.getActiveStep().popover.title;
+    draft.description = tourInstance.getActiveStep().popover.description;
+    draft.isLastStep = tourInstance.isLastStep();
+    draft.isFirstStep = tourInstance.isFirstStep();
+};
