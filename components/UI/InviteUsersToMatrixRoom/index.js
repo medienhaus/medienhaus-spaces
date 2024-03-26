e13334828a18be883f310310b792c95d9d342d6a
Marcel <79279756+aofn@users.noreply.github.com>
2024-03-26 15:45:51 +0100

    refactor: remove console.logs

--- components/UI/InviteUsersToMatrixRoom/index.js
+++ components/UI/InviteUsersToMatrixRoom/index.js
@@ -65,19 +65,14 @@ export const InviteUserToMatrixRoom = ({ roomId, trigger, promote }) => {
         const errors = [];
 
         const setPower = async (roomId, userId, powerLevel) => {
-            console.debug('changing power level for ' + userId);
             matrixClient.getStateEvent(roomId, 'm.room.power_levels', '').then(async (res) => {
                 const powerEvent = new MatrixEvent({
                     type: 'm.room.power_levels',
                     content: res,
                 });
 
-                try {
-                    // something here is going wrong for collab > 2
-                    await matrixClient.setPowerLevel(roomId, userId, powerLevel, powerEvent);
-                } catch (err) {
-                    console.error(err);
-                }
+                // something here is going wrong for collab > 2
+                return await matrixClient.setPowerLevel(roomId, userId, powerLevel, powerEvent);
             });
         };
 
