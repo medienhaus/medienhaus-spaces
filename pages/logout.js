b78f6b51df2ebffd2e4c5ddb6ddc7cd4abbd18d3
Robert Schnüll <mail@robertschnuell.de>
2024-04-03 14:26:24 +0200

    fixing onboarding logout bug which prevents tab

--- pages/logout.js
+++ pages/logout.js
@@ -2,19 +2,22 @@ import { useRouter } from 'next/router';
 import { useEffect } from 'react';
 
 import { useAuth } from '@/lib/Auth';
+import { useOnboarding } from '@/components/onboarding/onboardingContext';
 
 export default function Logout() {
     const router = useRouter();
     const auth = useAuth();
+    const onboarding = useOnboarding();
 
     useEffect(() => {
         const logoutAndForward = async () => {
+            onboarding.tourInstance.destroy(); // destroy the driverJS instance so that tab will work again on the /login route
             await auth.signout();
             router.push('/login');
         };
 
         logoutAndForward();
-    }, [auth, router]);
+    }, [auth, onboarding.tourInstance, router]);
 
     return null;
 }
